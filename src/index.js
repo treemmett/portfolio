a66b44b568e7625db86a63f1d55a75c3a52b95a2
Tregan Emmett <treemmett@gmail.com>
2017-07-31 15:14:29 -0600

    Added Parallax

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './index.css';
+
+const scrollEffect = () => {
+  const scroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const banner = document.getElementById('banner');
+
+  if(banner)
+    banner.style.backgroundPosition = '50% calc(50% - '+scroll/15+'px)';
+}
+
+window.addEventListener('load', ()=>{
+  scrollEffect();
+});
+
+window.addEventListener('scroll', scrollEffect);
