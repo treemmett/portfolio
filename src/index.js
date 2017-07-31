77e4b5fdef1ecac0a3e4569308f3bce2bf272ee2
Tregan Emmett <treemmett@gmail.com>
2017-07-31 15:21:00 -0600

    Added anchor shortcut

--- src/index.js
+++ src/index.js
@@ -10,6 +10,21 @@ const scrollEffect = () => {
 
 window.addEventListener('load', ()=>{
   scrollEffect();
+  document.getElementById('nav').addEventListener('click', navScroll);
 });
 
 window.addEventListener('scroll', scrollEffect);
+
+const navScroll = (i) => {
+  const start = window.pageYOffset;
+  const stop = document.getElementsByClassName('article')[0].offsetTop;
+  const distance = Math.max(start, stop) - Math.min(start, stop);
+
+  if(i > start)
+    return;
+
+  if(start < stop){
+    window.scrollBy(0, 10);
+    window.setTimeout(function(){navScroll(start)}, 800 / distance);
+  }
+}
