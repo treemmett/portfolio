8507518bdb68effc5e5ab3ed0ad0cad0be9cd60c
treemmett <treemmett@gmail.com>
2017-08-28 10:16:57 -0600

    Removed sun and trail

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -2,10 +2,7 @@ export function drawCanvas(){
   const canvas = document.querySelector('canvas');
   const c = canvas.getContext('2d');
   const stars = [];
-  const starTrail = [];
   const starFlickerMargin = 0.6;
-
-  let mouse = {x: 0, y: 0};
 
   function init(){
     canvas.height = document.body.clientHeight;
@@ -52,34 +49,7 @@ export function drawCanvas(){
     }
   }
 
-  class StarTrail{
-    constructor(x, y){
-      this.x = x;
-      this.y = y;
-      this.vX = (Math.random() - 0.5) * 3;
-      this.vY = (Math.random() - 0.5) * 3;
-      this.radius = Math.random() + 0.5;
-      this.draw();
-    }
-
-    draw(){
-      c.beginPath();
-      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      c.fillStyle = 'white';
-      c.fill();
-    }
-
-    update(){
-      this.x += this.vX;
-      this.y += this.vY;
-      this.radius -= 0.03;
-
-      if(this.radius > 0){
-        this.draw();
-      }
-    }
-  }
-
+  //Initial star renders
   for(let i = 0; i < 100; i++){
     stars.push(new Star());
   }
@@ -102,27 +72,8 @@ export function drawCanvas(){
       }
     }
 
-    //Draw sun
-    c.beginPath();
-    c.arc(0, 0, 100, 0, Math.PI * 2, false);
-    c.fillStyle = '#feeb08';
-    c.save();
-    c.shadowBlur = 5;
-    c.shadowOffsetY = 25;
-    c.shadowOffsetX = 25;
-    c.shadowColor = '#feeb08';
-    c.restore();
-    c.fill();
-
   }
   animate();
 
   window.addEventListener('resize', init);
-
-  canvas.addEventListener('mousemove', (e)=>{
-    mouse.x = e.layerX;
-    mouse.y = e.layerY;
-
-    starTrail.push(new StarTrail(e.layerX, e.layerY));
-  });
 }
