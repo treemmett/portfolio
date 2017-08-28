364516ace647423588a65d39d9efb7b1e7769529
treemmett <treemmett@gmail.com>
2017-08-28 12:16:01 -0600

    Added shooting stars

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -2,6 +2,7 @@ export function drawCanvas(){
   const canvas = document.querySelector('canvas');
   const c = canvas.getContext('2d');
   const stars = [];
+  const shootingStars = [];
   const starFlickerMargin = 0.6;
 
   function init(){
@@ -49,6 +50,35 @@ export function drawCanvas(){
     }
   }
 
+  class ShootingStar{
+    constructor(){
+      this.x = Math.random() * canvas.width;
+      this.y = Math.random() * canvas.height;
+      this.vX = (Math.random() - 0.5) * 10;
+      this.vY = (Math.random() - 0.5) * 10;
+      this.alpha = 1;
+
+      this.draw();
+    }
+
+    draw(){
+      c.beginPath();
+      c.arc(this.x, this.y, this.alpha, 0, Math.PI * 2, false);
+      c.fillStyle = 'rgba(255, 255, 255, '+this.alpha+')';
+      c.fill();
+    }
+
+    update(){
+      this.alpha -= 0.02;
+      this.x += this.vX;
+      this.y += this.vY;
+
+      if(this.alpha > 0){
+        this.draw();
+      }
+    }
+  }
+
   //Initial star renders
   for(let i = 0; i < 100; i++){
     stars.push(new Star());
@@ -61,6 +91,20 @@ export function drawCanvas(){
     //Draw stars
     for(let i in stars){
       stars[i].update();
+    }
+
+    //Update shooting stars
+    for(let i in shootingStars){
+      if(shootingStars[i].alpha > 0){
+        shootingStars[i].update();
+      }else{
+        shootingStars.splice(i, 1);
+      }
+    }
+
+    //Create shooting stars
+    if(Math.random() < 0.02){
+      shootingStars.push(new ShootingStar());
     }
 
   }
