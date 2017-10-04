e2d0aa28b64347355290ee737efb754afd208e58
treemmett <treemmett@gmail.com>
2017-10-04 13:54:11 -0600

    Added waves

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,10 +1,7 @@
 export function drawCanvas(){
   const canvas = document.querySelector('canvas');
   const c = canvas.getContext('2d');
-  const stars = [];
-  const shootingStars = [];
-  const flickerStars = [];
-  const starFlickerMargin = 0.6;
+  let waves = [];
 
   function init(){
     canvas.height = document.body.clientHeight;
@@ -12,151 +9,90 @@ export function drawCanvas(){
   }
   init();
 
-  class Star{
+  class Wave{
     constructor(){
-      this.x = Math.random() * canvas.width * 2;
-      this.y = Math.random() * canvas.height * 2;
+      this.points = [];
+      this.noPoints = Math.random() * 2 + 2;
+      this.finished = false;
 
-      this.realRadius = Math.random() + 0.2;
-      this.radius = Math.random() * this.realRadius;
-      this.grow = Math.random() > 0.5;
-      this.draw();
+      //Setup initial points
+      this.points.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        alpha: 0
+      });
     }
 
     draw(){
+      //Draw circle
+      for(let i in this.points){
+        c.beginPath();
+        c.arc(this.points[i].x, this.points[i].y, 3, 0, Math.PI * 2);
+        c.fillStyle = 'rgba(255,255,255,'+this.points[i].alpha*2+')';
+        c.fill();
+      }
+
+      //Draw line
       c.beginPath();
-      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      c.fillStyle = 'white';
-      c.fill();
+      for(let i in this.points){
+        c.lineTo(this.points[i].x, this.points[i].y);
+        c.strokeStyle = 'rgba(255, 255, 255, '+this.points[i].alpha+')';
+        c.stroke();
+      }
     }
 
     update(){
-      if(this.grow){
-        if(this.radius < this.realRadius + starFlickerMargin){
-          this.radius += 0.01;
-        }else{
-          this.grow = !this.grow;
+      if(this.finished){
+        //Decrease alpha for all point
+        for(let i in this.points){
+          this.points[i].alpha -= 0.01;
         }
+
+      }else if(this.points[this.points.length - 1].alpha < 1){
+        //Increase alpha for current point
+        this.points[this.points.length - 1].alpha += 0.01;
+
+      }else if(this.points.length < this.noPoints){
+        //Add new point
+        this.addPoint();
+
       }else{
-        if(this.radius > this.realRadius - starFlickerMargin && this.radius > 0.1){
-          this.radius -= 0.01;
-        }else{
-          this.grow = !this.grow;
-        }
+        //All points added, start fade out
+        this.finished = true;
       }
 
       this.draw();
     }
-  }
 
-  class ShootingStar{
-    constructor(){
-      this.x = Math.random() * canvas.width;
-      this.y = Math.random() * canvas.height;
-      this.vX = (Math.random() - 0.5) * 10;
-      this.vY = (Math.random() - 0.5) * 10;
-      this.alpha = 1;
+    addPoint(){
+      const oldX = this.points[this.points.length - 1].x;
+      const oldY = this.points[this.points.length - 1].y;
+      const radius = Math.random() * 100 + 20;
+      const angle = Math.random() * 360;
 
-      this.draw();
-    }
-
-    draw(){
-      c.beginPath();
-      c.arc(this.x, this.y, this.alpha, 0, Math.PI * 2, false);
-      c.fillStyle = 'rgba(255, 255, 255, '+this.alpha+')';
-      c.fill();
-    }
-
-    update(){
-      this.alpha -= 0.02;
-      this.x += this.vX;
-      this.y += this.vY;
-
-      if(this.alpha > 0){
-        this.draw();
-      }
+      this.points.push({
+        x: oldX + radius * Math.cos(angle),
+        y: oldY + radius * Math.sin(angle),
+        alpha: 0
+      });
     }
   }
 
-  class FlickerStars{
-    constructor(){
-      this.x = Math.random() * canvas.width;
-      this.y = Math.random() * canvas.height;
-      this.realRadius = Math.random() * 2;
-      this.radius = 0.1;
-      this.grow = true;
-      this.growthRate = (Math.random() * 0.05) + 0.01;
-      this.draw();
-    }
-
-    draw(){
-      c.beginPath();
-      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      c.fillStyle = 'rgba(255, 255, 255, '+this.radius+')';
-      c.fill();
-    }
-
-    update(){
-      if(this.grow){
-        if(this.radius < this.realRadius){
-          this.radius += this.growthRate;
-        }else{
-          this.grow = false;
-        }
-      }else{
-        if(this.radius > 0){
-          this.radius -= this.growthRate;
-        }
-      }
-
-      if(this.radius > 0){
-        this.draw();
-      }
-    }
-  }
-
-  //Initial star renders
-  for(let i = 0; i < 100; i++){
-    stars.push(new Star());
-  }
+  //Add first wave
+  waves.push(new Wave());
 
   function animate(){
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
 
-    //Draw stars
-    for(let i in stars){
-      stars[i].update();
+    for(let i in waves){
+      waves[i].update();
     }
 
-    //Update shooting stars
-    for(let i in shootingStars){
-      if(shootingStars[i].alpha > 0){
-        shootingStars[i].update();
-      }else{
-        shootingStars.splice(i, 1);
-      }
-    }
-
-    //Draw flickering stars
-    if(Math.random() < 0.15){
-      flickerStars.push(new FlickerStars());
-    }
-
-    //Update flickering stars
-    for(let i in flickerStars){
-      if(flickerStars[i].radius > 0){
-        flickerStars[i].update();
-      }else{
-        flickerStars.splice(i, 1);
-      }
-    }
-
-    //Create shooting stars
+    //Roll dice to add new wave
     if(Math.random() < 0.01){
-      shootingStars.push(new ShootingStar());
+      waves.push(new Wave());
     }
-
   }
   animate();
 
