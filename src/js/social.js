39792fdc77ca4166307c590fe75b3dcb863991b1
treemmett <treemmett@gmail.com>
2017-10-09 14:44:13 -0600

    Removed email copy input

--- src/js/social.js
+++ src/js/social.js
@@ -1,13 +1,14 @@
-//Add email to body
-window.addEventListener('load', function(){
-  const email = document.createElement('input');
-  email.value = 'hello@tregan.me';
-  document.body.append(email);
-
-  //Copy email to clipboard
+//Copy email to clipboard
+window.addEventListener('DOMContentLoaded', function(){
   document.getElementById('email').addEventListener('click', function(){
+    const email = document.createElement('input');
+    email.style.position = 'fixed';
+    email.style.opacity = '0';
+    email.value = 'hello@tregan.me';
+    document.body.appendChild(email);
     email.select();
     document.execCommand('copy');
     notification('Email copied to clipboard');
+    email.remove();
   });
 });
