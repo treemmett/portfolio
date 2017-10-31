68dc522c0a31bd7483d7cc3d4ca5cbaf82067b6b
treemmett <treemmett@gmail.com>
2017-10-31 14:27:34 -0600

    Removed below the fold contents

--- src/index.js
+++ src/index.js
@@ -2,26 +2,5 @@ import './js/canvas_waves';
 import './js/canvas_mesh';
 import './js/notification';
 import './js/banner';
-import ContactForm from './js/contactForm';
 
 import './index.scss';
-
-const contactF = new ContactForm(document.forms.contact);
-
-window.addEventListener('load', ()=>{
-  //Add contact events
-  document.querySelector('.contact .btn').addEventListener('click', ()=>{contactF.submit()});
-  for(let i = 0; i < document.forms.contact.elements.length; i++){
-    document.forms.contact.elements[i].addEventListener('input', ()=>{contactF.checkInput()});
-  }
-
-  //Load reCAPTCHA
-  if(grecaptcha){
-    grecaptcha.render(document.getElementById('grecaptcha'), {
-      sitekey: '6Lea3iwUAAAAAHaZPlnEhUVb-nCOYFrzO93Ac52t',
-      size: 'invisible',
-      badge: 'inline',
-      callback: ()=>{contactF.submit()}
-    });
-  }
-});
