b5477209d2d2d7d4210c4c86251812b00cbaa7bb
Tregan <tregan@tregan.me>
2022-01-08 01:31:54 -0700

    chore: add linter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import { AppProps } from 'next/app';
+import { FC } from 'react';
 import '../styles/globals.scss';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+// eslint-disable-next-line react/jsx-props-no-spreading
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
+import { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.scss';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Tregan</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export const Home: NextPage = () => (
+  <div className={styles.container}>
+    <Head>
+      <title>Tregan</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome</h1>
-      </main>
-    </div>
-  );
-}
+    <main className={styles.main}>
+      <h1 className={styles.title}>Welcome</h1>
+    </main>
+  </div>
+);
+
+export default Home;
