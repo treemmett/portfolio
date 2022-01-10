e68cdfaf7140c2e0e3f6240d73ac3192d82ce8a2
Tregan <tregan@tregan.me>
2022-01-10 04:27:04 -0700

    chore(post): dynamic image sizes

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,24 +1,27 @@
 import Image from 'next/image';
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import styles from './Post.module.scss';
 
 export interface PostProps {
-  height?: number;
   title?: string;
   text?: string;
   photo?: string;
-  width?: number;
 }
 
-export const Post: FC<PostProps> = ({ height = 700, title = 'Portugal!', width = 1000 }) => (
-  <div className={styles.post}>
-    <Image
-      alt={title}
-      className={styles.photo}
-      src={`https://picsum.photos/${width}/${height}`}
-      width={width}
-      height={height}
-    />
-    <div className={styles.content}>{title}</div>
-  </div>
-);
+export const Post: FC<PostProps> = ({ title = 'Portugal!' }) => {
+  const width = useMemo(() => Math.floor(Math.random() * 1000 + 400), []);
+  const height = useMemo(() => Math.floor(Math.random() * 1000 + 400), []);
+
+  return (
+    <div className={styles.post}>
+      <Image
+        alt={title}
+        className={styles.photo}
+        src={`https://picsum.photos/${width}/${height}`}
+        width={width}
+        height={height}
+      />
+      <div className={styles.content}>{title}</div>
+    </div>
+  );
+};
