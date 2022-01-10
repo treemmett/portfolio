262a757907c04c80e1bec5e47b53e227c0efd9f4
Tregan <tregan@tregan.me>
2022-01-10 01:18:48 -0700

    feat(gallery): horizontal layout

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,8 +1,9 @@
 import { FC } from 'react';
 import { Post } from './Post';
+import styles from './Gallery.module.scss';
 
 export const Gallery: FC = () => (
-  <section>
+  <section className={styles.gallery}>
     {new Array(10).fill(null).map((_, i) => (
       // eslint-disable-next-line react/no-array-index-key
       <Post key={i} />
