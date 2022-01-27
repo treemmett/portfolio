4e8a09ee03ef2600162fe749a18cc00d7f067a7e
Tregan <tregan@tregan.me>
2022-01-27 15:03:30 +0100

    refactor: localize global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 import { FC } from 'react';
-import '../styles/globals.scss';
+import './_app.scss';
 
 // eslint-disable-next-line react/jsx-props-no-spreading
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
