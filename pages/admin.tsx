ea59a7c79a2c46168a1821417c3983a33cd63c6e
Tregan <tregan@tregan.me>
2022-01-27 14:36:42 +0100

    feat(admin): add title input

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,6 +1,7 @@
 import cx from 'classnames';
 import { NextPage } from 'next';
 import { ChangeEventHandler, useCallback, useState } from 'react';
+import { Input } from '../components/Input';
 import styles from './admin.module.scss';
 
 const Admin: NextPage = () => {
@@ -29,6 +30,7 @@ const Admin: NextPage = () => {
         )}
         <input accept="image/*" id="image" onChange={handleFileChange} type="file" />
       </label>
+      <Input className={styles.input} label="Title" />
     </div>
   );
 };
