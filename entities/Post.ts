9073fe4eb781f721a6c7183916644c03440e355c
Tregan <tregan@tregan.me>
2022-01-29 18:18:37 +0100

    feat(post): add resized and blurred images

--- entities/Post.ts
+++ entities/Post.ts
@@ -1,5 +1,9 @@
+import { tmpdir } from 'os';
+import { join } from 'path';
 import { plainToClass } from 'class-transformer';
+import Jimp from 'jimp';
 import { Entity, getRepository, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { v4 } from 'uuid';
 import { Photo } from './Photo';
 
 const TABLE_NAME = 'posts';
@@ -17,8 +21,33 @@ export class Post {
   }
 
   public static async upload(filePath: string): Promise<Post> {
-    const photo = await Photo.upload(filePath);
-    const post = plainToClass(Post, { photos: [photo] });
+    const path = join(tmpdir(), v4());
+
+    const image: Jimp = await new Promise((res, rej) => {
+      Jimp.read(filePath, (err, img) => {
+        if (err) {
+          rej(err);
+        } else {
+          res(img);
+        }
+      });
+    });
+
+    await Promise.all([
+      image.clone().scaleToFit(1000, 1000).writeAsync(`${path}_1000`),
+      image.clone().scaleToFit(500, 500).writeAsync(`${path}_500`),
+      image.clone().scaleToFit(1000, 1000).blur(15).writeAsync(`${path}_b_1000`),
+      image.clone().scaleToFit(500, 500).blur(15).writeAsync(`${path}_b_500`),
+    ]);
+
+    const post = plainToClass(Post, {
+      photos: await Promise.all(
+        [filePath, `${path}_1000`, `${path}_500`, `${path}_b_1000`, `${path}_b_500`].map((p) =>
+          Photo.upload(p)
+        )
+      ),
+    });
+
     await Post.repository().save(post);
     return post;
   }
