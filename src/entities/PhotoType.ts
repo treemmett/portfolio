87d11d8c9eec223a83fb88850cb6c724222660a2
Tregan <tregan@tregan.me>
2022-05-29 23:13:03 -0700

    fix: remove server process from shared enums

--- src/entities/PhotoType.ts
+++ src/entities/PhotoType.ts
@@ -1,11 +1,5 @@
-import { registerEnumType } from 'type-graphql';
-
 export enum PhotoType {
   BLURRED,
   ORIGINAL,
   SCALED,
 }
-
-registerEnumType(PhotoType, {
-  name: 'PhotoType',
-});
--- src/middleware/graphql.ts
+++ src/middleware/graphql.ts
@@ -1,8 +1,14 @@
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import { ApolloServer } from 'apollo-server-micro';
-import { buildSchema } from 'type-graphql';
+import { buildSchema, registerEnumType } from 'type-graphql';
+import { PhotoType } from '../entities/PhotoType';
 import { PhotoResolver } from '../resolvers/PhotoResolver';
 import { PostResolver } from '../resolvers/PostResolver';
+
+// TODO should this live here?
+registerEnumType(PhotoType, {
+  name: 'PhotoType',
+});
 
 export const apolloServer = new ApolloServer({
   csrfPrevention: process.env.NODE_ENV === 'production',
