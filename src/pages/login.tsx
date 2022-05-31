70e454c2277ec0b13affed3a96da8031a6772773
Tregan <tregan@tregan.me>
2022-05-31 00:13:12 -0700

    feat(auth): add login states

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,23 +3,27 @@ import { useEffect, useState } from 'react';
 import { CSRF_STORAGE_KEY, isAuthenticated } from '../utils/authentication';
 import { ErrorCode } from '../utils/errors';
 
+enum LOGIN_STATES {
+  default,
+  authorizing,
+  denied,
+  error,
+  success,
+}
+
 const Login: NextPage = () => {
-  const [denied, setDenied] = useState(false);
+  const [state, setState] = useState(LOGIN_STATES.default);
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
 
     if (params.get('error')) {
-      const githubError = params.get('error');
-
-      if (githubError === 'access_denied') {
-        setDenied(true);
-      }
-
+      setState(params.get('error') === 'access_denied' ? LOGIN_STATES.denied : LOGIN_STATES.error);
       return;
     }
 
     if (isAuthenticated()) {
+      setState(LOGIN_STATES.success);
       return;
     }
 
@@ -38,19 +42,24 @@ const Login: NextPage = () => {
     })
       .then((r) => r.json())
       .then((data) => {
-        // invalid auth code, try again
-        if (data.error === ErrorCode.invalid_auth_code) {
-          window.location.replace(
-            `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`
-          );
+        if (data.error) {
+          // invalid auth code, try again
+          if (data.error === ErrorCode.invalid_auth_code) {
+            window.location.replace(
+              `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`
+            );
+          } else {
+            setState(LOGIN_STATES.error);
+          }
           return;
         }
 
         localStorage.setItem(CSRF_STORAGE_KEY, JSON.stringify(data));
+        setState(LOGIN_STATES.success);
       });
   }, []);
 
-  if (denied)
+  if (state === LOGIN_STATES.denied)
     return (
       <div>
         Authorization with GitHub was denied. Try again, or don't try again. I really don't care.
