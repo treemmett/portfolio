13f687b07c5b5775c76938248d7f66fa88753202
Tregan <tregan@tregan.me>
2022-06-03 23:39:50 -0700

    feat: add breakpoint constants

--- src/utils/pixels.ts
+++ src/utils/pixels.ts
@@ -1,3 +1,5 @@
+import screen from '../styles/screen.module.scss';
+
 export function getRemValue(): number {
   if (typeof window !== 'undefined') {
     return parseFloat(getComputedStyle(document.documentElement).fontSize);
@@ -9,3 +11,14 @@ export function getRemValue(): number {
 export function toPx(px: number): string {
   return `${px}px`;
 }
+
+export function pxToNumber(string: string): number {
+  if (!string) return 0;
+
+  return Number(string.replace(/px$/, ''));
+}
+
+export const smMin = pxToNumber(screen.smMin);
+export const mdMin = pxToNumber(screen.mdMin);
+export const lgMin = pxToNumber(screen.lgMin);
+export const xlMin = pxToNumber(screen.xlMin);
