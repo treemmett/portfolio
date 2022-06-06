3694ce730c9cbfa130dec7fda668229e238aebf5
Tregan <tregan@tregan.me>
2022-06-06 00:49:56 -0700

    feat: add social icons

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,11 @@
 import { FC } from 'react';
+import { ReactComponent as GitHub } from '../icons/github.svg';
+import { ReactComponent as Instagram } from '../icons/instagram.svg';
+import { ReactComponent as LinkedIn } from '../icons/linkedin.svg';
+import { ReactComponent as User } from '../icons/user.svg';
 import { Config } from '../utils/config';
 import styles from './About.module.scss';
+import { Button } from './Button';
 
 export interface AboutProps {
   backdrop?: boolean;
@@ -12,5 +17,19 @@ export const About: FC<AboutProps> = ({ backdrop }) =>
   ) : (
     <main className={styles.main}>
       <h2>Hi, I'm {Config.NEXT_PUBLIC_NAME}</h2>
+      <div className={styles.social}>
+        <Button>
+          <GitHub />
+        </Button>
+        <Button>
+          <Instagram />
+        </Button>
+        <Button>
+          <LinkedIn />
+        </Button>
+        <Button>
+          <User />
+        </Button>
+      </div>
     </main>
   );
