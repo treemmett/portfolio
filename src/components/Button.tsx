7c5d1d01320fca9c87e51db9a262dc4025d081b1
Tregan <tregan@tregan.me>
2022-06-06 01:31:03 -0700

    feat(button): proxy click handler

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,5 @@
 import cx from 'classnames';
-import { FC } from 'react';
+import { FC, MouseEventHandler } from 'react';
 import styles from './Button.module.scss';
 
 export type ButtonTypes = 'default' | 'primary';
@@ -7,15 +7,24 @@ export type ButtonTypes = 'default' | 'primary';
 export interface ButtonProps {
   className?: string;
   disabled?: boolean;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
   /** button is of submit type */
   submit?: boolean;
   type?: ButtonTypes;
 }
 
-export const Button: FC<ButtonProps> = ({ children, className, disabled, submit, type }) => (
+export const Button: FC<ButtonProps> = ({
+  children,
+  className,
+  disabled,
+  onClick,
+  submit,
+  type,
+}) => (
   <button
     className={cx(styles.button, className, { [styles.primary]: type === 'primary' })}
     disabled={disabled}
+    onClick={onClick}
     type={submit ? 'submit' : 'button'}
   >
     {children}
