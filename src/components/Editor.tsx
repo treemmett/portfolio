b970a4d0aa74ea587fd8d6a4540e286200d8351f
Tregan <tregan@tregan.me>
2022-07-01 22:11:40 +0100

    feat: add editor modal

---
+++ src/components/Editor.tsx
@@ -0,0 +1,95 @@
+import cx from 'classnames';
+import { useRouter } from 'next/router';
+import { ChangeEventHandler, FC, FormEventHandler, useCallback, useState } from 'react';
+import { Button } from './Button';
+import { useDataStore } from './DataStore';
+import styles from './Editor.module.scss';
+import { Input } from './Input';
+import { Modal } from './Modal';
+
+enum UploadState {
+  default,
+  uploading,
+}
+
+export const Editor: FC = () => {
+  const router = useRouter();
+  const { apiClient } = useDataStore();
+  const [imageData, setImageData] = useState('');
+  const handleFileChange: ChangeEventHandler<HTMLInputElement> = useCallback((e) => {
+    const file = e.currentTarget.files[0];
+    if (!file) {
+      setImageData('');
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+      setImageData(reader.result.toString());
+    });
+    reader.readAsDataURL(file);
+  }, []);
+
+  const [state, setState] = useState(UploadState.default);
+  const uploadPost: FormEventHandler<HTMLFormElement> = useCallback(
+    async (e) => {
+      try {
+        // don't duplicate requests
+        if (state === UploadState.uploading) return;
+
+        setState(UploadState.uploading);
+        e.preventDefault();
+        const form = e.target as HTMLFormElement;
+
+        await apiClient.post('/api/post', new FormData(form));
+
+        form.reset();
+        setImageData('');
+        router.push({ query: { newPost: undefined } });
+      } catch (err) {
+        console.error(err?.response?.data?.error || err);
+        alert(['Upload failed', err?.response?.data?.message].join(' - '));
+      } finally {
+        setState(UploadState.default);
+      }
+    },
+    [apiClient, router, state]
+  );
+
+  return (
+    <Modal
+      onClose={() => router.push({ query: { newPost: undefined } })}
+      open={router.query.newPost?.length > 0}
+    >
+      {router.query.newPost?.length > 0 && (
+        <div className={styles.container}>
+          <form className={styles.form} onSubmit={uploadPost}>
+            <label className={cx(styles.picker, { [styles.selected]: imageData })} htmlFor="image">
+              {imageData ? (
+                <img alt="selection preview" className={styles.preview} src={imageData} />
+              ) : (
+                <div>Pick an image</div>
+              )}
+              <input
+                accept="image/*"
+                id="image"
+                name="file"
+                onChange={handleFileChange}
+                type="file"
+              />
+            </label>
+            <Input className={styles.input} label="Title" />
+            <Button
+              className={styles.input}
+              disabled={state === UploadState.uploading}
+              type="primary"
+              submit
+            >
+              {state === UploadState.uploading ? 'Uploading...' : 'Post'}
+            </Button>
+          </form>
+        </div>
+      )}
+    </Modal>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { About } from '../components/About';
 import { Button } from '../components/Button';
 import { useDataStore } from '../components/DataStore';
+import { Editor } from '../components/Editor';
 import { Gallery } from '../components/Gallery';
 import { LightBox } from '../components/LightBox';
 import { AuthorizationScopes } from '../entities/Jwt';
@@ -11,6 +13,7 @@ import styles from './home.module.scss';
 
 export const Home: NextPage = () => {
   const { session } = useDataStore();
+  const { push } = useRouter();
 
   return (
     <div className={styles.container}>
@@ -22,9 +25,15 @@ export const Home: NextPage = () => {
 
       <About />
 
-      {session.hasPermission(AuthorizationScopes.post) && <Button type="fab">New Post</Button>}
+      {session.hasPermission(AuthorizationScopes.post) && (
+        <Button onClick={() => push({ query: { newPost: true } })} type="fab">
+          New Post
+        </Button>
+      )}
 
       <LightBox />
+
+      <Editor />
     </div>
   );
 };
