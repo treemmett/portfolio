e9ced019fedf36710935e2a9cc9f857906f72caa
Tregan <tregan@tregan.me>
2022-07-03 18:17:52 +0100

    test(auth): assert logout functionality

--- cypress/e2e/authentication.cy.ts
+++ cypress/e2e/authentication.cy.ts
@@ -14,4 +14,20 @@ describe('authentication', () => {
     cy.contains('Welcome back');
     cy.get('[data-testid="new post"]').should('not.exist');
   });
+
+  it('should persist credential across sessions', () => {
+    cy.login('treemmett');
+    cy.contains('Welcome back');
+    cy.visit('/');
+    cy.contains('Welcome back');
+  });
+
+  it('log user out', () => {
+    cy.login('treemmett');
+    cy.contains('Welcome back');
+    cy.get('[data-testid="logout"]').click();
+    cy.contains("Hi, I'm Tregan");
+    cy.visit('/');
+    cy.contains('Welcome back').should('not.exist');
+  });
 });
