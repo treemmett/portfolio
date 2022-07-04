34f610668251ecbb3f123c6f963d99c042b72dd3
Tregan <tregan@tregan.me>
2022-07-04 12:58:03 +0100

    chore: remove photo endpoints

--- src/pages/api/[...path].ts
+++ src/pages/api/[...path].ts
@@ -2,7 +2,6 @@ import 'reflect-metadata';
 import '../../../cypress/mock';
 import { serialize } from 'cookie';
 import { PageConfig } from 'next';
-import { Photo } from '../../entities/Photo';
 import { Post } from '../../entities/Post';
 import { nextConnect } from '../../middleware/nextConnect';
 import { authorizeGitHub } from '../../utils/auth';
@@ -21,12 +20,10 @@ export default nextConnect
     );
     res.send(accessToken);
   })
-  .get('/api/photo', async (req, res) => res.json(await Photo.getAll()))
   .get('/api/post', async (req, res) => {
     const id = toString(req.query.id);
     res.send(await (id ? Post.get(id) : Post.getAll()));
   })
-  .post('/api/photo', async (req, res) => res.json(await Photo.upload(req.files.file.filepath)))
   .post('/api/post', async (req, res) => res.json(await Post.upload(req.files.file.filepath)));
 
 export const config: PageConfig = {
