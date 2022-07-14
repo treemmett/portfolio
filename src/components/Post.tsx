ae61b39fe6410ecb3ed9bb2911705d151a9d0759
Tregan <tregan@tregan.me>
2022-07-15 00:15:17 +0100

    perf: use css for dimensions

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -2,10 +2,9 @@ import cx from 'classnames';
 import Image from 'next/future/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { FC, useCallback, useEffect, useRef, useState } from 'react';
+import { FC, useEffect, useRef } from 'react';
 import { PhotoType } from '../entities/PhotoType';
 import type { Post as PostEntity } from '../entities/Post';
-import { getRemValue, mdMin, scaleDimensions, toPx } from '../utils/pixels';
 import { useDataStore } from './DataStore';
 import styles from './Post.module.scss';
 
@@ -14,67 +13,8 @@ export interface PostProps {
   priority?: boolean;
 }
 
-const MAX_HEIGHT = 40 * getRemValue();
-const HEIGHT = 0.6;
-
-const WIDTH = 0.9;
-
 export const Post: FC<PostProps> = ({ post, priority }) => {
   const ref = useRef<HTMLAnchorElement>();
-  const [height, setHeight] = useState(toPx(0));
-  const [width, setWidth] = useState(toPx(0));
-
-  const scaleImage = useCallback(() => {
-    const isMobile = window.innerWidth <= mdMin;
-    const [photo] = post.photos;
-
-    if (isMobile) {
-      const scaled = window.innerWidth * WIDTH;
-      const [scaledW, scaledH] = scaleDimensions(
-        photo.width,
-        photo.height,
-        {
-          w: scaled,
-        },
-        ref.current?.parentElement,
-        getRemValue()
-      );
-      setWidth(toPx(scaledW));
-      setHeight(toPx(scaledH));
-    } else {
-      const scaled = window.innerHeight * HEIGHT;
-      const [scaledW, scaledH] = scaleDimensions(
-        photo.width,
-        photo.height,
-        {
-          h: scaled > MAX_HEIGHT ? MAX_HEIGHT : scaled,
-        },
-        ref.current?.parentElement,
-        getRemValue() * 2
-      );
-      setWidth(toPx(scaledW));
-      setHeight(toPx(scaledH));
-    }
-  }, [post]);
-
-  useEffect(() => {
-    window.addEventListener('resize', scaleImage);
-    scaleImage();
-
-    return () => window.removeEventListener('resize', scaleImage);
-  }, [scaleImage]);
-
-  /**
-   * This effect adds the width/height transition after the post has been
-   * rendered, preventing an animation on the initial load
-   */
-  const [unsetTransition, setUnsetTransition] = useState(true);
-  useEffect(() => {
-    const loaded = [height, width].every((dimension) => dimension !== '0px');
-    if (loaded) {
-      setUnsetTransition(true);
-    }
-  }, [height, width]);
 
   const { query } = useRouter();
   const { setLightBox } = useDataStore();
@@ -92,17 +32,13 @@ export const Post: FC<PostProps> = ({ post, priority }) => {
         className={cx(styles.post, { [styles.displayed]: query.post === post.id })}
         href="#foo"
         ref={ref}
-        style={{ height, transition: unsetTransition && 'unset', width }}
       >
         <div
           className={styles['image-wrapper']}
-          style={{ height, transition: unsetTransition && 'unset', width }}
+          style={{ backgroundColor: `rgb(${post.red}, ${post.green}, ${post.blue})` }}
         >
-          <div
-            className={cx(styles.placeholder, styles.photo)}
-            style={{ backgroundColor: `rgb(${post.red}, ${post.green}, ${post.blue})` }}
-          />
           <Image
+            alt="some post"
             blurDataURL={image.thumbnailURL}
             className={styles.photo}
             height={image.height}
--- src/utils/pixels.ts
+++ src/utils/pixels.ts
@@ -1,28 +1,6 @@
-import screen from '../styles/screen.module.scss';
-import { isBrowser } from './isBrowser';
-
-export function getRemValue(): number {
-  if (isBrowser()) {
-    return parseFloat(getComputedStyle(document.documentElement).fontSize);
-  }
-
-  return 0;
-}
-
 export function toPx(px: number): string {
   return `${px}px`;
 }
-
-export function pxToNumber(string: string): number {
-  if (!string) return 0;
-
-  return Number(string.replace(/px$/, ''));
-}
-
-export const smMin = pxToNumber(screen.smMin);
-export const mdMin = pxToNumber(screen.mdMin);
-export const lgMin = pxToNumber(screen.lgMin);
-export const xlMin = pxToNumber(screen.xlMin);
 
 export function scaleDimensions(
   w: number,
