1d967f575844ccf5a51f91f58a7aee970120b5ce
Tregan <tregan@tregan.me>
2022-07-15 05:15:32 +0100

    refactor(data store): remove callback

--- src/components/DataStore.tsx
+++ src/components/DataStore.tsx
@@ -4,7 +4,6 @@ import {
   FC,
   MutableRefObject,
   PropsWithChildren,
-  useCallback,
   useContext,
   useEffect,
   useMemo,
@@ -20,9 +19,9 @@ export interface DataStoreContext {
   deletePost(id: string): Promise<void>;
   destroySession: () => void;
   lightBox?: MutableRefObject<HTMLElement>;
-  login: () => void;
+  login(): void;
   session: Session;
-  setLightBox: (lightBox?: DataStoreContext['lightBox']) => void;
+  setLightBox(lightBox?: DataStoreContext['lightBox']): void;
 }
 
 export const dataStoreContext = createContext<DataStoreContext>({
@@ -60,58 +59,56 @@ export const DataStoreProvider: FC<PropsWithChildren> = ({ children }) => {
 
   const [lightBox, setLightBox] = useState<MutableRefObject<HTMLElement>>();
 
-  const login = useCallback(() => {
-    if (session?.expiration > new Date()) return;
-
-    const messageHandler = async (event: MessageEvent<OAuthSuccessMessage | OAuthErrorMessage>) => {
-      if (event.origin !== window.location.origin) {
-        throw new Error('Message failed cross-origin check');
-      }
-
-      event.source.postMessage({
-        type: 'OAUTH_CLOSE',
-      } as OAuthCloseMessage);
-
-      window.removeEventListener('message', messageHandler);
-
-      if (event.data.type === 'OAUTH_ERROR') {
-        throw new Error(event.data.payload);
-      }
-
-      if (event.data.type === 'OAUTH_CODE') {
-        const loginResponse = await apiClient.post('/login', { code: event.data.payload });
-        localStorage.setItem(ACCESS_TOKEN_STORAGE_KEY, loginResponse.data);
-        setSession(new Session(loginResponse.data));
-      }
-    };
-
-    window.addEventListener('message', messageHandler);
-
-    window.open(
-      `https://github.com/login/oauth/authorize?client_id=${Config.NEXT_PUBLIC_GITHUB_CLIENT_ID}`,
-      'oauth',
-      `popup,width=500,height=750,left=${global.screen.width / 2 - 250}`
-    );
-  }, [apiClient, session?.expiration]);
-
-  const destroySession = useCallback(() => {
-    localStorage.removeItem(ACCESS_TOKEN_STORAGE_KEY);
-    setSession(new Session());
-  }, []);
-
   const contextValue = useMemo<DataStoreContext>(
     () => ({
       apiClient,
       async deletePost(id) {
         await apiClient.delete(`/post/${encodeURIComponent(id)}`);
       },
-      destroySession,
+      destroySession() {
+        localStorage.removeItem(ACCESS_TOKEN_STORAGE_KEY);
+        setSession(new Session());
+      },
       lightBox,
-      login,
+      login() {
+        if (session?.expiration > new Date()) return;
+
+        const messageHandler = async (
+          event: MessageEvent<OAuthSuccessMessage | OAuthErrorMessage>
+        ) => {
+          if (event.origin !== window.location.origin) {
+            throw new Error('Message failed cross-origin check');
+          }
+
+          event.source.postMessage({
+            type: 'OAUTH_CLOSE',
+          } as OAuthCloseMessage);
+
+          window.removeEventListener('message', messageHandler);
+
+          if (event.data.type === 'OAUTH_ERROR') {
+            throw new Error(event.data.payload);
+          }
+
+          if (event.data.type === 'OAUTH_CODE') {
+            const loginResponse = await apiClient.post('/login', { code: event.data.payload });
+            localStorage.setItem(ACCESS_TOKEN_STORAGE_KEY, loginResponse.data);
+            setSession(new Session(loginResponse.data));
+          }
+        };
+
+        window.addEventListener('message', messageHandler);
+
+        window.open(
+          `https://github.com/login/oauth/authorize?client_id=${Config.NEXT_PUBLIC_GITHUB_CLIENT_ID}`,
+          'oauth',
+          `popup,width=500,height=750,left=${global.screen.width / 2 - 250}`
+        );
+      },
       session,
       setLightBox,
     }),
-    [apiClient, destroySession, lightBox, login, session]
+    [apiClient, lightBox, session]
   );
 
   return <dataStoreContext.Provider value={contextValue}>{children}</dataStoreContext.Provider>;
