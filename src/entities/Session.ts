995966adee8b3e86ceadb102628c8ffb7b3e11be
Tregan <tregan@tregan.me>
2022-07-16 01:29:16 +0100

    perf(session): remove jwt dep

--- src/entities/Session.ts
+++ src/entities/Session.ts
@@ -1,6 +1,11 @@
-import { decode } from 'jsonwebtoken';
 import { isBrowser } from '../utils/isBrowser';
 import { ACCESS_TOKEN_STORAGE_KEY, AuthorizationScopes, Jwt } from './Jwt';
+
+function decodeToken(accessToken: string): Jwt {
+  const [, payload] = accessToken.split('.');
+  const json = window.atob(payload);
+  return JSON.parse(json);
+}
 
 export class Session {
   public expiration?: Date;
@@ -12,7 +17,7 @@ export class Session {
   constructor(public accessToken?: string) {
     if (!accessToken) return;
 
-    const { exp, scp, sub } = decode(accessToken) as Jwt;
+    const { exp, scp, sub } = decodeToken(accessToken);
 
     this.expiration = new Date(exp * 1000);
     this.scope = scp;
