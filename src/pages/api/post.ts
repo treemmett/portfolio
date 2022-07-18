a662b47c10723f80d4b6f92290756f2192cab76e
Tregan <tregan@tregan.me>
2022-07-18 14:42:33 +0100

    fix(posts): revalidate after methods

--- src/pages/api/post.ts
+++ src/pages/api/post.ts
@@ -3,23 +3,18 @@ import { Post } from '../../entities/Post';
 import { nextConnect } from '../../middleware/nextConnect';
 import { logger } from '../../utils/logger';
 
-export default nextConnect()
-  .post(async (req, res) => {
-    const post = await Post.upload(
-      req.files?.file.filepath,
-      req.body.title,
-      req.body.location,
-      req.body.date
-    );
-    logger.info('Post created, revalidating cache');
-    await res.revalidate('/');
-    logger.info('Cache revalidated');
-    res.json(post);
-  })
-  .delete(async (req, res) => {
-    await Post.delete(req.body.id);
-    res.end();
-  });
+export default nextConnect().post(async (req, res) => {
+  const post = await Post.upload(
+    req.files?.file.filepath,
+    req.body.title,
+    req.body.location,
+    req.body.date
+  );
+  logger.info('Post created, revalidating cache');
+  await res.revalidate('/');
+  logger.info('Cache revalidated');
+  res.json(post);
+});
 
 export const config: PageConfig = {
   api: {
--- src/pages/api/post/[id].ts
+++ src/pages/api/post/[id].ts
@@ -1,15 +1,20 @@
 import { PageConfig } from 'next';
 import { Post } from '../../../entities/Post';
 import { nextConnect } from '../../../middleware/nextConnect';
+import { logger } from '../../../utils/logger';
 import { toString } from '../../../utils/queryParam';
 
 export default nextConnect()
   .delete(async (req, res) => {
     await Post.delete(toString(req.query.id));
+    logger.info('Post deleted, revalidating cache');
+    await res.revalidate('/');
     res.end();
   })
   .put(async (req, res) => {
     const post = await Post.update(toString(req.query.id), req.body);
+    logger.info('Post updated, revalidating cache');
+    await res.revalidate('/');
     res.send(post);
   });
 
