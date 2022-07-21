c935ae2838f48d7f06d51e921f9d6f1f97e94528
Tregan <tregan@tregan.me>
2022-07-21 14:12:35 +0200

    feat(timeline): save marker to storage

--- src/pages/timeline.tsx
+++ src/pages/timeline.tsx
@@ -1,8 +1,16 @@
-import { Map } from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 import { NextPage } from 'next';
 import { useEffect, useRef } from 'react';
 import { Config } from '../utils/config';
 import styles from './timeline.module.scss';
+
+const MARKERS_KEY = 'markers';
+
+function getMarkers(): LngLat[] {
+  const storage = localStorage.getItem(MARKERS_KEY);
+  return storage ? JSON.parse(storage) : [];
+}
 
 const Timeline: NextPage = () => {
   const mapContainer = useRef<HTMLDivElement>();
@@ -18,7 +26,16 @@ const Timeline: NextPage = () => {
         zoom: 9,
       });
 
-      map.current.resize();
+      map.current.on('click', (e) => {
+        const markers = getMarkers();
+        markers.push(e.lngLat);
+        new Marker().setLngLat(e.lngLat).addTo(map.current);
+        localStorage.setItem(MARKERS_KEY, JSON.stringify(markers));
+      });
+
+      getMarkers().forEach((lngLat) => {
+        new Marker().setLngLat(lngLat).addTo(map.current);
+      });
     }
   }, [map]);
 
