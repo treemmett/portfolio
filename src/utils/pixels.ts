65db42b22597c63feec7693019daebc221368d2f
Tregan <tregan@tregan.me>
2022-08-05 02:19:31 +0200

    feat: add dark mode utils

--- src/utils/pixels.ts
+++ src/utils/pixels.ts
@@ -1,3 +1,5 @@
+import { isBrowser } from './isBrowser';
+
 export function toPx(px: number): string {
   return `${px}px`;
 }
@@ -52,4 +54,29 @@ export function scaleDimensions(
   }
 
   return [width, height];
+}
+
+/**
+ * Checks if system is running in dark-mode
+ */
+export function isDarkMode(): boolean {
+  if (!isBrowser()) return false;
+
+  return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
+
+/**
+ * Subscribe to system color theme changes
+ * @param listener Callback when theme changes
+ * @returns Unsubscribe callback
+ */
+export function listenForDarkModeChange(listener: (isDarkMode: boolean) => void): () => void {
+  const handler = (e: MediaQueryListEvent) => {
+    listener(e.matches);
+  };
+
+  const query = window.matchMedia('(prefers-color-scheme: dark)');
+
+  query.addEventListener('change', handler);
+  return () => query.removeEventListener('change', handler);
 }
