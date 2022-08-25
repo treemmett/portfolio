32d75f89091394bc91fb052ac5065d1915d6fb44
Tregan <tregan@tregan.me>
2022-08-25 10:57:49 +0200

    test: set interceptor adapter

--- src/mock.ts
+++ src/mock.ts
@@ -1,6 +1,10 @@
+import axios from 'axios';
+import adapter from 'axios/lib/adapters/http';
 import nock from 'nock';
 
 if (process.env.MOCK === 'true') {
+  axios.defaults.adapter = adapter;
+
   nock('https://github.com')
     .post('/login/oauth/access_token')
     .reply(200, (uri, body: Record<string, string>) => ({ access_token: body.code }))
