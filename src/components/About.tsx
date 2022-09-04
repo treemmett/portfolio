4e4deb93c1f24a0524687b1a48d27f38373be52d
Tregan <tregan@tregan.me>
2022-09-05 01:05:16 +0200

    fix(timeline): add authorization

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -35,7 +35,6 @@ export const About: FC = () => {
     );
 
     const intervalId = setInterval(() => {
-      console.log({ popup });
       if (popup.closed) {
         dispatch({ type: 'LOGOUT' });
         clearInterval(intervalId);
--- src/pages/api/timeline.ts
+++ src/pages/api/timeline.ts
@@ -1,7 +1,10 @@
+import { AuthorizationScopes } from '../../entities/Jwt';
 import { Marker } from '../../entities/Marker';
+import { Session } from '../../entities/Session';
 import { nextConnect } from '../../middleware/nextConnect';
 
 export default nextConnect().post(async (req, res) => {
+  await Session.authorizeRequest(req, AuthorizationScopes.post);
   const marker = await Marker.checkIn(req.body);
   res.send(marker);
 });
