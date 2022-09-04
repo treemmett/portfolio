bd80db7104dc4ced575bfd2f5e8530bc1e0e1303
Tregan <tregan@tregan.me>
2022-09-05 01:05:37 +0200

    fix(timeline): revalidate static props

--- src/pages/api/timeline.ts
+++ src/pages/api/timeline.ts
@@ -2,9 +2,11 @@ import { AuthorizationScopes } from '../../entities/Jwt';
 import { Marker } from '../../entities/Marker';
 import { Session } from '../../entities/Session';
 import { nextConnect } from '../../middleware/nextConnect';
+import { i18nRevalidate } from '../../utils/revalidate';
 
 export default nextConnect().post(async (req, res) => {
   await Session.authorizeRequest(req, AuthorizationScopes.post);
   const marker = await Marker.checkIn(req.body);
+  await i18nRevalidate('/timeline', res);
   res.send(marker);
 });
