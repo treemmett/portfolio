e3506940da61f2e92aa5ae2526ac319885c3fdbd
Tregan <tregan@tregan.me>
2022-09-05 18:02:23 +0200

    fix(casing): remove broken regex

--- src/utils/casing.ts
+++ src/utils/casing.ts
@@ -1,8 +1,3 @@
 export function splitCase(input: string): string {
-  return !input || input.indexOf(' ') >= 0
-    ? input
-    : (input.charAt(0).toUpperCase() + input.substring(1))
-        .split(/(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])/g)
-        .map((x) => x.replace(/([0-9]+)/g, '$1 '))
-        .join(' ');
+  return input.replaceAll(/[A-Z-_&](?=[a-z0-9]+)|[A-Z-_&]+(?![a-z0-9])/g, ' $&').trim();
 }
