aebfaa6193774887ed17d3936c9845a91bc6d7a3
Tregan <tregan@tregan.me>
2022-09-10 20:54:31 +0200

    feat(resume): add social icons

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -3,7 +3,20 @@ import { join } from 'path';
 import { GetStaticProps, NextPage } from 'next';
 import getConfig from 'next/config';
 import { Resume as ResumeType } from 'resume';
+import { ReactComponent as At } from '../icons/at-sign.svg';
+import { ReactComponent as GitHub } from '../icons/github.svg';
+import { ReactComponent as Instagram } from '../icons/instagram.svg';
+import { ReactComponent as LinkIcon } from '../icons/link.svg';
+import { ReactComponent as LinkedIn } from '../icons/linkedin.svg';
+import { ReactComponent as Twitter } from '../icons/twitter.svg';
 import styles from './resume.module.scss';
+
+const ProfileIcons = {
+  github: GitHub,
+  instagram: Instagram,
+  linkedin: LinkedIn,
+  twitter: Twitter,
+};
 
 export const getStaticProps: GetStaticProps = async () => {
   const { root } = getConfig().serverRuntimeConfig;
@@ -33,13 +46,24 @@ const Resume: NextPage<ResumeProps> = ({ resume }) => (
         <section>
           <h3>About</h3>
           <hr />
-          <a href={`mailto:${resume.basics.email}`}>{resume.basics.email}</a>
-          <a href={resume.basics.url}>{resume.basics.url}</a>
-          {resume.basics.profiles.map((profile) => (
-            <a href={profile.url} key={profile.network}>
-              /{profile.username}
-            </a>
-          ))}
+          <h5>
+            <At />
+            <a href={`mailto:${resume.basics.email}`}>{resume.basics.email}</a>
+          </h5>
+          <h5>
+            <LinkIcon />
+            <a href={resume.basics.url}>{resume.basics.url}</a>
+          </h5>
+          {resume.basics.profiles.map((profile) => {
+            const Icon = ProfileIcons[profile.network?.toLowerCase()];
+
+            return (
+              <h5 key={profile.network}>
+                {Icon && <Icon />}
+                <a href={profile.url}>/{profile.username}</a>
+              </h5>
+            );
+          })}
         </section>
       </aside>
     </main>
