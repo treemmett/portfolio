3aef0719ce7ae456286d3c4e62b79850cc982337
Tregan <tregan@tregan.me>
2022-09-10 21:35:36 +0200

    fix(resume): add mobile layout

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -41,30 +41,31 @@ const Resume: NextPage<ResumeProps> = ({ resume }) => (
       <h2>{resume.basics.label}</h2>
     </header>
 
-    <main>
-      <aside>
-        <section>
-          <h3>About</h3>
-          <hr />
-          <h5>
-            <At />
-            <a href={`mailto:${resume.basics.email}`}>{resume.basics.email}</a>
-          </h5>
-          <h5>
-            <LinkIcon />
-            <a href={resume.basics.url}>{resume.basics.url}</a>
-          </h5>
-          {resume.basics.profiles.map((profile) => {
-            const Icon = ProfileIcons[profile.network?.toLowerCase()];
+    <main className={styles.grid}>
+      <aside className={styles.contact}>
+        <h3>Contact</h3>
+        <hr />
+        <h5>
+          <At />
+          <a href={`mailto:${resume.basics.email}`}>{resume.basics.email}</a>
+        </h5>
+        <h5>
+          <LinkIcon />
+          <a href={resume.basics.url}>{resume.basics.url}</a>
+        </h5>
+        {resume.basics.profiles.map((profile) => {
+          const Icon = ProfileIcons[profile.network?.toLowerCase()];
 
-            return (
-              <h5 key={profile.network}>
-                {Icon && <Icon />}
-                <a href={profile.url}>/{profile.username}</a>
-              </h5>
-            );
-          })}
-        </section>
+          return (
+            <h5 key={profile.network}>
+              {Icon && <Icon />}
+              <a href={profile.url}>/{profile.username}</a>
+            </h5>
+          );
+        })}
+      </aside>
+
+      <aside className={styles.skills}>
         {resume.skills?.map((skill) => (
           <section key={skill.name}>
             <h3>{skill.name}</h3>
@@ -76,7 +77,13 @@ const Resume: NextPage<ResumeProps> = ({ resume }) => (
         ))}
       </aside>
 
-      <aside>
+      <aside className={styles.about}>
+        <h3>About</h3>
+        <hr />
+        <p>{resume.basics.summary}</p>
+      </aside>
+
+      <aside className={styles.experience}>
         <section>
           <h3>Experience</h3>
           <hr />
