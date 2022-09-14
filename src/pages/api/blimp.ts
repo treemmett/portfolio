5a53fdd66d98c4e3568cb9cc84d56be3adccefc5
Tregan <tregan@tregan.me>
2022-09-15 00:12:15 +0200

    feat(analytics): add geolocation provider

--- src/pages/api/blimp.ts
+++ src/pages/api/blimp.ts
@@ -3,6 +3,7 @@ import { getClientIp } from 'request-ip';
 import { Session } from '@entities/Session';
 import { nextConnect } from '@middleware/nextConnect';
 import { Config } from '@utils/config';
+import { geolocation } from '@utils/geolocation';
 import { logger } from '@utils/logger';
 import { toString } from '@utils/queryParam';
 
@@ -21,26 +22,44 @@ export default nextConnect().post(async (req, res) => {
   // return immediately, client doesn't care
   res.send({ status: 'ok' });
 
-  const session = await Session.fromRequest(req).catch(() => false);
+  const ip = getClientIp(req);
+
+  const [session, geoData] = await Promise.all([
+    Session.fromRequest(req).catch(() => false),
+    geolocation(ip),
+  ]);
 
   const body: InsightsRequest = {
     id: req.body.id,
     parameters: {
       ...req.body.parameters,
-      locationCity: req.headers['x-vercel-ip-city']
+      ip,
+      username: typeof session === 'boolean' ? '<no session>' : session?.username || '<no session>',
+      vercelCity: req.headers['x-vercel-ip-city']
         ? decodeURIComponent(toString(req.headers['x-vercel-ip-city']))
         : undefined,
-      locationCountry: req.headers['x-vercel-ip-country'],
-      locationIp: getClientIp(req),
-      locationLanguage: req.headers['accept-language'],
-      locationLatitude: req.headers['x-vercel-ip-latitude'],
-      locationLongitude: req.headers['x-vercel-ip-longitude'],
-      locationRegion: req.headers['x-vercel-ip-country-region'],
-      locationTimezone: req.headers['x-vercel-ip-timezone'],
-      username: typeof session === 'boolean' ? '<no session>' : session?.username || '<no session>',
+      vercelCountry: req.headers['x-vercel-ip-country'],
+      vercelLanguage: req.headers['accept-language'],
+      vercelLatitude: req.headers['x-vercel-ip-latitude'],
+      vercelLongitude: req.headers['x-vercel-ip-longitude'],
+      vercelRegion: req.headers['x-vercel-ip-country-region'],
+      vercelTimezone: req.headers['x-vercel-ip-timezone'],
     },
     projectId: Config.INSIGHTS_TOKEN,
   };
+
+  if (geoData) {
+    body.parameters.geoCity = geoData.city;
+    body.parameters.geoCountry = geoData.country_code2;
+    body.parameters.geoContinent = geoData.continent_code;
+    body.parameters.geoState = geoData.state_prov;
+    body.parameters.geoDistrict = geoData.district;
+    body.parameters.geoZipcode = geoData.zipcode;
+    body.parameters.geoLatitude = geoData.latitude;
+    body.parameters.geoLongitude = geoData.longitude;
+    body.parameters.geoLanguages = geoData.languages;
+    body.parameters.geoTimezone = geoData.time_zone.offset.toString();
+  }
 
   await axios.post('https://getinsights.io/app/tics', body).catch((err) => {
     logger.error(err, 'Analytics failed');
---
+++ src/utils/geolocation.ts
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import { Config } from './config';
+import { logger } from './logger';
+import { Country } from '@lib/countryCodes';
+
+interface GeolocationResponse {
+  ip: string;
+  hostname: string;
+  continent_code: string;
+  continent_name: string;
+  country_code2: Country;
+  country_code3: string;
+  country_name: string;
+  country_capital: string;
+  state_prov: string;
+  district: string;
+  city: string;
+  // cspell:word zipcode
+  zipcode: string;
+  latitude: string;
+  longitude: string;
+  is_eu: number;
+  calling_code: string;
+  country_tld: string;
+  languages: string;
+  country_flag: string;
+  isp: string;
+  connection_type: string;
+  organization: string;
+  asn: string;
+  currency: {
+    code: string;
+    name: string;
+    symbol: string;
+  };
+  time_zone: {
+    name: string;
+    offset: number;
+    current_time: string;
+    current_time_unix: number;
+    is_dst: boolean;
+    dst_savings: number;
+  };
+}
+
+export async function geolocation(ip: string): Promise<false | GeolocationResponse> {
+  try {
+    if (['127.0.0.1', '0.0.0.0', '::ffff:127.0.0.1', '::1', '0:0:0:0:0:0:0:1'].includes(ip)) {
+      return false;
+    }
+
+    const { data } = await axios.get<GeolocationResponse>('https://api.ipgeolocation.io/ipgeo', {
+      params: { apiKey: Config.GEOLOCATION_KEY, ip },
+    });
+
+    return data;
+  } catch (err) {
+    logger.error(err, 'Failed to load geolocation data');
+    return false;
+  }
+}
