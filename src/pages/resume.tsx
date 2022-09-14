05ab301d96584ad67ed2b2ffac719b3b7d999bf8
Tregan <tregan@tregan.me>
2022-09-14 23:45:49 +0200

    fix(resume): section layout

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -36,88 +36,80 @@ export interface ResumeProps {
 
 const Resume: NextPage<ResumeProps> = ({ resume }) => (
   <div className={styles.page}>
-    <header>
+    <header className={styles.header}>
       <h1>{resume.basics.name}</h1>
       <h2>{resume.basics.label}</h2>
     </header>
 
-    <main className={styles.grid}>
-      <aside className={styles.contact}>
-        <h3>Contact</h3>
-        <hr />
-        <h5>
-          <At />
-          <a href={`mailto:${resume.basics.email}`} rel="noreferrer" target="_blank">
-            {resume.basics.email}
+    <section className={styles.contact}>
+      <h3>Contact</h3>
+      <hr />
+      <a href={`mailto:${resume.basics.email}`} rel="noreferrer" target="_blank">
+        <At />
+        <h5>{resume.basics.email}</h5>
+      </a>
+      <a href={resume.basics.url} rel="noreferrer" target="_blank">
+        <LinkIcon />
+        <h5>{resume.basics.url}</h5>
+      </a>
+      {resume.basics.profiles.map((profile) => {
+        const Icon = ProfileIcons[profile.network?.toLowerCase()];
+
+        return (
+          <a href={profile.url} key={profile.network} rel="noreferrer" target="_blank">
+            {Icon && <Icon />}
+            <h5>/{profile.username}</h5>
           </a>
-        </h5>
-        <h5>
-          <LinkIcon />
-          <a href={resume.basics.url} rel="noreferrer" target="_blank">
-            {resume.basics.url}
-          </a>
-        </h5>
-        {resume.basics.profiles.map((profile) => {
-          const Icon = ProfileIcons[profile.network?.toLowerCase()];
+        );
+      })}
+    </section>
 
-          return (
-            <h5 key={profile.network}>
-              {Icon && <Icon />}
-              <a href={profile.url} rel="noreferrer" target="_blank">
-                /{profile.username}
-              </a>
-            </h5>
-          );
-        })}
-      </aside>
-
-      <aside className={styles.skills}>
-        {resume.skills?.map((skill) => (
-          <section key={skill.name}>
-            <h3>{skill.name}</h3>
-            <hr />
-            {skill.keywords.map((keyword) => (
-              <h5 key={keyword}>{keyword}</h5>
-            ))}
-          </section>
-        ))}
-      </aside>
-
-      <aside className={styles.languages}>
-        <h3>Languages</h3>
-        <hr />
-        {resume?.languages?.map((language) => (
-          <section key={language.language}>
-            <h5>{language.language}</h5>
-            <h6>{language.fluency}</h6>
-          </section>
-        ))}
-      </aside>
-
-      <aside className={styles.about}>
-        <h3>About</h3>
-        <hr />
-        <p>{resume.basics.summary}</p>
-      </aside>
-
-      <aside className={styles.experience}>
-        <section>
-          <h3>Experience</h3>
+    <section className={styles.skills}>
+      {resume.skills?.map((skill) => (
+        <section key={skill.name}>
+          <h3>{skill.name}</h3>
           <hr />
-          {resume.work?.map((work) => (
-            <section className={styles.work} key={work.name}>
-              <h5>{`${work.startDate} - ${work.endDate || 'Present'}`}</h5>
-              <h4>{work.name}</h4>
-              <ul>
-                {work.highlights.map((highlight) => (
-                  <li key={highlight}>{highlight}</li>
-                ))}
-              </ul>
-            </section>
+          {skill.keywords.map((keyword) => (
+            <h5 key={keyword}>{keyword}</h5>
           ))}
         </section>
-      </aside>
-    </main>
+      ))}
+    </section>
+
+    <section className={styles.about}>
+      <h3>About</h3>
+      <hr />
+      <p>{resume.basics.summary}</p>
+    </section>
+
+    <section className={styles.languages}>
+      <h3>Languages</h3>
+      <hr />
+      <div className={styles.list}>
+        {resume?.languages?.map((language) => (
+          <div className={styles.language} key={language.language}>
+            <h5>{language.language}</h5>
+            <h6>{language.fluency}</h6>
+          </div>
+        ))}
+      </div>
+    </section>
+
+    <section className={styles.experience}>
+      <h3>Experience</h3>
+      <hr />
+      {resume.work?.map((work) => (
+        <div className={styles.work} key={work.name}>
+          <h5>{`${work.startDate} - ${work.endDate || 'Present'}`}</h5>
+          <h4>{work.name}</h4>
+          <ul>
+            {work.highlights.map((highlight) => (
+              <li key={highlight}>{highlight}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </section>
   </div>
 );
 
