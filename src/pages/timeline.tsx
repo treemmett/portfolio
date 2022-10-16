3d9c9956179f8600e02b9a781359c5567e21f358
Austin Ftacnik <austin@ftacnik.com>
2022-10-15 17:16:45 -0700

    refactor(timeline): use bounding coordinates util

--- src/pages/timeline.tsx
+++ src/pages/timeline.tsx
@@ -11,6 +11,7 @@ import { AuthorizationScopes } from '@entities/Jwt';
 import { Marker as MarkerEntity } from '@entities/Marker';
 import { Country } from '@lib/countryCodes';
 import { countryFlags } from '@lib/countryFlags';
+import { boundingCoordinates } from '@utils/boundingCoordinates';
 import { splitCase } from '@utils/casing';
 import { Config } from '@utils/config';
 import {
@@ -30,27 +31,7 @@ export interface TimelineProps {
 export const getStaticProps: GetStaticProps<DefaultState & TimelineProps> = async ({ locale }) => {
   const markers = await MarkerEntity.getAll();
 
-  const ne: LngLatLike = markers.length
-    ? { lat: markers[0].lat, lng: markers[0].lng }
-    : { lat: 42.35, lng: -70.9 };
-  const sw = { ...ne };
-  [...markers].slice(-10).forEach((lngLat) => {
-    if (ne.lng < lngLat.lng) {
-      ne.lng = lngLat.lng;
-    }
-
-    if (ne.lat < lngLat.lat) {
-      ne.lat = lngLat.lat;
-    }
-
-    if (sw.lng > lngLat.lng) {
-      sw.lng = lngLat.lng;
-    }
-
-    if (sw.lat > lngLat.lat) {
-      sw.lat = lngLat.lat;
-    }
-  });
+  const { sw, ne } = boundingCoordinates(markers);
 
   // filter country flags to avoid sending massive payload to client
   const countryMappings = Object.fromEntries(Object.entries(Country).map((a) => a.reverse()));
