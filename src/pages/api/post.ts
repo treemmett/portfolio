43f130b617a334e539ddc4f8a72d07166b2fc6d4
Tregan <tregan@tregan.me>
2022-10-29 13:44:26 -0400

    feat(post): add get endpoint

--- src/pages/api/post.ts
+++ src/pages/api/post.ts
@@ -6,15 +6,18 @@ import { logger } from '@utils/logger';
 import { i18nRevalidate } from '@utils/revalidate';
 
 export default nextConnect()
-  .use(Session.authorizeRequest(AuthorizationScopes.post))
-  .patch(async (req, res) => {
+  .get(async (req, res) => {
+    const posts = await Post.getAll();
+    res.send(posts);
+  })
+  .patch(Session.authorizeRequest(AuthorizationScopes.post), async (req, res) => {
     await i18nRevalidate('/', res);
   })
-  .post(async (req, res) => {
+  .post(Session.authorizeRequest(AuthorizationScopes.post), async (req, res) => {
     const token = await Post.requestUploadToken(req.body.location, req.body.title, req.body.date);
     res.send(token);
   })
-  .put(async (req, res) => {
+  .put(Session.authorizeRequest(AuthorizationScopes.post), async (req, res) => {
     const post = await Post.processUpload(req.body.token);
     res.send(post);
     logger.info('Post created, revalidating cache');
