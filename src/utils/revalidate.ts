dd45b8f5ecfcfbcb4e2a528b64cb90fb4c04dac9
Tregan <tregan@tregan.me>
2022-11-23 09:34:26 +1300

    fix(revalidation): don't await

--- src/utils/revalidate.ts
+++ src/utils/revalidate.ts
@@ -3,12 +3,14 @@ import { i18n } from '../../next-i18next.config';
 import { logger } from './logger';
 
 export async function i18nRevalidate(urlPath: string, res: NextApiResponse): Promise<void> {
-  await Promise.all(
+  Promise.all(
     [urlPath, ...i18n.locales.map((locale) => `/${locale}${urlPath}`.replace(/(\/*$)/, ''))].map(
       async (path) => {
         logger.info('Revalidating', path);
-        await res.revalidate(path);
-        logger.info('Revalidation successful', path);
+        res
+          .revalidate(path)
+          .catch((err) => logger.error('Revalidation failed', err, { path }))
+          .then(() => logger.info('Revalidation successful', path));
       }
     )
   );
