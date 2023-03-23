5c8af4f6e545011c9b171091770d6f235a560050
Tregan <tregan@tregan.me>
2023-03-23 01:46:40 +0000

    fix(mosaic): add editor

--- src/components/Mosaic.tsx
+++ src/components/Mosaic.tsx
@@ -1,16 +1,28 @@
-import { FC } from 'react';
+import dynamic from 'next/dynamic';
+import { FC, Suspense } from 'react';
 import { useDataStore } from './DataStore';
 import styles from './Mosaic.module.scss';
 import { Tile } from './Tile';
+import { AuthorizationScopes } from '@entities/Jwt';
+
+const DynamicEditor = dynamic(() => import('./Editor').then((mod) => mod.Editor));
 
 export const Mosaic: FC = () => {
-  const { posts } = useDataStore();
+  const { posts, session } = useDataStore();
 
   return (
-    <div className={styles.mosaic}>
-      {posts.map((post) => (
-        <Tile key={post.id} post={post} />
-      ))}
-    </div>
+    <>
+      <div className={styles.mosaic}>
+        {posts.map((post) => (
+          <Tile key={post.id} post={post} />
+        ))}
+      </div>
+
+      {session.hasPermission(AuthorizationScopes.post) && (
+        <Suspense fallback="Loading...">
+          <DynamicEditor />
+        </Suspense>
+      )}
+    </>
   );
 };
