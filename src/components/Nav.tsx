a9180c856dd43415d881e2288f18219e5a9d2f2a
Tregan <tregan@tregan.me>
2023-03-23 19:16:13 +0000

    feat(nav): add social media

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { Anchor } from './Anchor';
 import styles from './Nav.module.scss';
 import { useSite } from '@lib/site';
 
@@ -8,6 +9,25 @@ export const Nav: FC = () => {
   return (
     <header className={styles.nav}>
       <h1>{site?.name}</h1>
+
+      <nav>
+        {site?.facebook && (
+          <Anchor href={`https://facebook.com/${encodeURIComponent(site.facebook)}`}>
+            Facebook
+          </Anchor>
+        )}
+        {site?.github && (
+          <Anchor href={`https://github.com/${encodeURIComponent(site.github)}`}>GitHub</Anchor>
+        )}
+        {site?.imdb && <Anchor href={`https://www.imdb.com/name/${site.imdb}/`}>IMDb</Anchor>}
+        {site?.instagram && (
+          <Anchor href={`https://www.instagram.com/${site.instagram}/`}>Instagram</Anchor>
+        )}
+        {site?.twitter && <Anchor href={`https://twitter.com/${site.instagram}/`}>Twitter</Anchor>}
+        {site?.linkedIn && (
+          <Anchor href={`https://www.linkedin.com/in/${site.linkedIn}/`}>LinkedIn</Anchor>
+        )}
+      </nav>
     </header>
   );
 };
