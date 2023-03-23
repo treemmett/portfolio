5c32205b33ba6fcc28562f647cf4f16b24a71f8a
Tregan <tregan@tregan.me>
2023-03-23 18:11:54 +0000

    fix(post): add request validation

--- src/pages/api/post/[id].ts
+++ src/pages/api/post/[id].ts
@@ -1,3 +1,4 @@
+import { Joi, celebrate } from 'celebrate';
 import { AuthorizationScopes } from '@entities/Jwt';
 import { Post } from '@entities/Post';
 import { Session } from '@entities/Session';
@@ -7,14 +8,15 @@ import { toString } from '@utils/queryParam';
 import { i18nRevalidate } from '@utils/revalidate';
 
 export default nextConnect()
+  .use(celebrate({ query: { id: Joi.string().required() } }))
   .delete(Session.authorizeRequest(AuthorizationScopes.delete), async (req, res) => {
-    await Post.delete(toString(req.query.id));
+    await Post.delete(toString(req.query.id as string));
     logger.info('Post deleted, revalidating cache');
     await i18nRevalidate('/', res);
     res.end();
   })
   .patch(Session.authorizeRequest(AuthorizationScopes.post), async (req, res) => {
-    const post = await Post.update(toString(req.query.id), req.body);
+    const post = await Post.update(toString(req.query.id as string), req.body);
     logger.info('Post updated, revalidating cache');
     await i18nRevalidate('/', res);
     res.send(post);
