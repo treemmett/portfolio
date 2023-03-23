4b545d80cf1e088e6c8ae9cb002cde50ed5e6d71
Tregan <tregan@tregan.me>
2023-03-23 00:50:58 +0000

    feat(tile): open light box

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { FC, useMemo } from 'react';
 import styles from './Tile.module.scss';
 import { PhotoType } from '@entities/PhotoType';
 import { Post } from '@entities/Post';
+import { formatDate } from '@utils/date';
 
 export interface TileProps {
   post: Post;
@@ -15,14 +17,24 @@ export const Tile: FC<TileProps> = ({ post }) => {
   );
 
   return (
-    <Image
-      alt={post.title}
-      blurDataURL={image.thumbnailURL}
-      className={styles.tile}
-      height={image.height}
-      placeholder="blur"
-      src={image.url}
-      width={image.width}
-    />
+    <Link
+      aria-label={[post.title, post.location, formatDate(post.created)]
+        .filter((i) => !!i)
+        .join(', ')}
+      href={{ query: { post: post.id } }}
+      scroll={false}
+      passHref
+      shallow
+    >
+      <Image
+        alt={post.title}
+        blurDataURL={image.thumbnailURL}
+        className={styles.tile}
+        height={image.height}
+        placeholder="blur"
+        src={image.url}
+        width={image.width}
+      />
+    </Link>
   );
 };
