163cc3a640496bcda71dd756fd21d933e1f8dc55
Tregan <tregan@tregan.me>
2023-03-25 22:59:44 +0000

    fix(axiom): ignore if unset

--- src/utils/axiom.ts
+++ src/utils/axiom.ts
@@ -2,13 +2,15 @@ import axios from 'axios';
 import { Config } from './config';
 
 export async function axiom(dataset: string, data: Record<string, unknown>): Promise<void> {
-  await axios.post(
-    `https://cloud.axiom.co/api/v1/datasets/${encodeURIComponent(dataset)}/ingest`,
-    [data],
-    {
-      headers: {
-        authorization: `Bearer ${Config.AXIOM_TOKEN}`,
-      },
-    }
-  );
+  if (Config.AXIOM_TOKEN) {
+    await axios.post(
+      `https://cloud.axiom.co/api/v1/datasets/${encodeURIComponent(dataset)}/ingest`,
+      [data],
+      {
+        headers: {
+          authorization: `Bearer ${Config.AXIOM_TOKEN}`,
+        },
+      }
+    );
+  }
 }
