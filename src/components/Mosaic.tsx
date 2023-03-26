fe8fc960df3a3a53a2dbaf45617d7b841704934a
Tregan <tregan@tregan.me>
2023-03-26 17:15:33 +0100

    feat(mosaic): add no photos text

--- src/components/Mosaic.tsx
+++ src/components/Mosaic.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'next-i18next';
 import { FC } from 'react';
 import styles from './Mosaic.module.scss';
 import { Tile } from './Tile';
@@ -5,6 +6,10 @@ import { usePosts } from '@lib/posts';
 
 export const Mosaic: FC = () => {
   const { posts } = usePosts();
+  const { t } = useTranslation();
+
+  if (!posts?.length)
+    return <div className={styles.placeholder}>{t('Upload some photos to get started')}</div>;
 
   return (
     <div className={styles.mosaic}>
