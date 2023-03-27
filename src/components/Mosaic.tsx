27d6e40ab26338d13bf8a207d0d9bccbf293a9dd
Tregan <tregan@tregan.me>
2023-03-27 18:33:34 +0100

    fix(mosaic): add generic empty message

--- src/components/Mosaic.tsx
+++ src/components/Mosaic.tsx
@@ -3,13 +3,21 @@ import { FC } from 'react';
 import styles from './Mosaic.module.scss';
 import { Tile } from './Tile';
 import { usePosts } from '@lib/posts';
+import { useSite } from '@lib/site';
+import { useUser } from '@lib/user';
 
 export const Mosaic: FC = () => {
+  const { user } = useUser();
+  const { site } = useSite();
   const { posts } = usePosts();
   const { t } = useTranslation();
 
   if (!posts?.length)
-    return <div className={styles.placeholder}>{t('Upload some photos to get started')}</div>;
+    return (
+      <div className={styles.placeholder}>
+        {site?.owner.id === user?.id ? t('Upload some photos to get started') : t('No photos')}
+      </div>
+    );
 
   return (
     <div className={styles.mosaic}>
