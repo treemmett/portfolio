8c0b7500933f8ff8511b7700d23436c748251772
Tregan <tregan@tregan.me>
2023-04-06 20:33:19 +0300

    fix(stats): use session

--- src/pages/api/photos/stats.ts
+++ src/pages/api/photos/stats.ts
@@ -2,8 +2,9 @@ import { Photo } from '@entities/Photo';
 import { User } from '@entities/User';
 import { nextConnect } from '@middleware/nextConnect';
 
-export default nextConnect().get(async (req, res) => {
-  const user = await User.findById('53e0af33-67b7-4f3f-ade1-0340a84d5e6b');
-  const data = await Photo.getStats(user);
-  res.send(data);
-});
+export default nextConnect()
+  .use(User.authorize())
+  .get(async (req, res) => {
+    const data = await Photo.getStats(req.user);
+    res.send(data);
+  });
