3f324a88722a3cf4b72cd012590361179344ec90
Tregan <tregan@tregan.me>
2023-09-23 06:04:55 +0200

    refactor(mosaic): use tailwind

--- src/app/gallery/Mosaic.tsx
+++ src/app/gallery/Mosaic.tsx
@@ -1,3 +1,4 @@
+import cx from 'classnames';
 import styles from './Mosaic.module.scss';
 import { Tile } from './Tile';
 import { getPosts } from '@lib/getPosts';
@@ -6,7 +7,7 @@ export async function Mosaic() {
   const posts = await getPosts('tregan');
 
   return (
-    <div className={styles.mosaic}>
+    <div className={cx('p-4 gap-4', styles.mosaic)}>
       {posts?.map((post, i) => <Tile key={post.id} post={post} priority={i < 5} />)}
     </div>
   );
