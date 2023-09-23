16ea7b6520f35d0a3b04768c19987b427cb4ee25
Tregan <tregan@tregan.me>
2023-09-23 08:47:43 +0200

    fix(gallery): move to root

--- src/app/(gallery)/layout.tsx
+++ src/app/(gallery)/layout.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from 'react';
 import { Nav } from '@components/Nav';
-import { Mosaic } from 'src/app/gallery/Mosaic';
+import { Mosaic } from 'src/app/(gallery)/Mosaic';
 
 export default async function GalleryPage({ children }: PropsWithChildren) {
   return (
