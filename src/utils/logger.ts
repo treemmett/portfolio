b4e6b1b5ac67aeb1befa2701fc8292af312c9276
Tregan <tregan@tregan.me>
2023-09-25 20:48:20 +0200

    chore: disable logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,6 +4,7 @@ import { Config } from './config';
 const isProd = Config.NODE_ENV === 'production';
 
 export const logger = pino({
+  enabled: false,
   level: 'trace',
   redact: {
     paths: isProd ? ['req.headers.cookie', 'res.headers["set-cookie"]'] : [],
