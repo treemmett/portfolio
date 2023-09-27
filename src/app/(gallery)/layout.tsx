1d6fc61cc99b15a21d5d5f998986032f3bed838b
Tregan <tregan@tregan.me>
2023-09-28 02:46:28 +0300

    fix(gallery): remove mosaic layout

--- src/app/(gallery)/layout.tsx
+++ src/app/(gallery)/layout.tsx
@@ -1,15 +1,20 @@
-import { PropsWithChildren, ReactNode } from 'react';
+import cx from 'classnames';
+import { PropsWithChildren } from 'react';
+import styles from './Mosaic.module.scss';
+import { Tile } from './Tile';
 import { Nav } from '@components/Nav';
+import { getPosts } from '@lib/getPosts';
 
-export default async function GalleryPage({
-  children,
-  mosaic,
-}: PropsWithChildren<{ mosaic: ReactNode }>) {
+export default async function GalleryPage({ children }: PropsWithChildren) {
+  const posts = await getPosts('tregan');
+
   return (
     <>
       <Nav />
       {children}
-      {mosaic}
+      <div className={cx('p-4 gap-4', styles.mosaic)}>
+        {posts?.map((post, i) => <Tile key={post.id} post={post} priority={i < 5} />)}
+      </div>
     </>
   );
 }
---
+++ src/app/(gallery)/loading.tsx
@@ -0,0 +1,32 @@
+import cx from 'classnames';
+import styles from './Mosaic.module.scss';
+import { Nav } from '@components/Nav';
+import { Spinner } from '@components/Spinner';
+import { toPx } from '@utils/pixels';
+
+function randomClamp(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+export default async function Mosaic() {
+  return (
+    <>
+      <Nav />
+
+      <div className={cx('p-4 gap-4', styles.mosaic)}>
+        {new Array(25).fill(null).map((_, i) => (
+          <div
+            className="bg-red-400 max-w-full max-h-full mb-4 flex justify-center items-center"
+            // eslint-disable-next-line react/no-array-index-key
+            key={i}
+            style={{
+              height: toPx(randomClamp(200, 400)),
+            }}
+          >
+            <Spinner />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
