1c6fabbe5030d154dfbbe8c67ab906291483e262
Tregan <tregan@tregan.me>
2023-09-28 03:02:26 +0300

    fix(settings): allow only for owner

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,5 +1,16 @@
+import { notFound } from 'next/navigation';
 import { SettingsForm } from './SettingsForm';
+import { getUser } from '@app/getUser';
+import { getSite } from '@lib/getSite';
+
+export const dynamic = 'force-dynamic';
 
 export default async function SettingsPage() {
+  const [user, site] = await Promise.all([getUser().catch(() => {}), getSite()]);
+
+  if (site?.ownerId !== user?.id) {
+    notFound();
+  }
+
   return <SettingsForm />;
 }
