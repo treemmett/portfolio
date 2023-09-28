30994a13eee957601e3cd38586b9a7ed08609d59
Tregan <tregan@tregan.me>
2023-09-28 23:10:35 +0300

    chore: remove legacy seed scripts

--- src/scripts/seedDown.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import 'reflect-metadata';
-import 'tsconfig-paths/register';
-
-import { AppDataSource } from '@utils/dataSource';
-
-AppDataSource.initialize()
-  .then(() => AppDataSource.undoLastMigration())
-  .then(() => AppDataSource.destroy());
