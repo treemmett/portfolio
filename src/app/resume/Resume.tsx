77fe3e3c1a957d87134a3c7243df7dec6b1c229c
Tregan <tregan@tregan.me>
2023-10-10 05:06:05 +0300

    feat(resume): add generate link

--- src/app/resume/Resume.tsx
+++ src/app/resume/Resume.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { FC, PropsWithChildren } from 'react';
 import {
   AtSign,
@@ -11,6 +12,7 @@ import {
 import { Resume as ResumeType } from 'resume';
 import styles from './resume.module.scss';
 import { Anchor } from '@components/Anchor';
+import { useTranslation } from '@utils/translation';
 
 const Section: FC<PropsWithChildren<{ className?: string; name?: string }>> = ({
   children,
@@ -26,110 +28,121 @@ const Section: FC<PropsWithChildren<{ className?: string; name?: string }>> = ({
 
 const ExternalLink: FC = () => <ExternalLinkIcon className={styles['external-link']} />;
 
-export const Resume: FC<{ resume: ResumeType }> = ({ resume }) => (
-  <div className={styles.page}>
-    <header className={styles.header}>
-      {resume.basics?.name && <h1>{resume.basics.name}</h1>}
-      {resume.basics?.label && <h2>{resume.basics.label}</h2>}
-    </header>
+export const Resume: FC<{ resume: ResumeType }> = ({ resume }) => {
+  const { t } = useTranslation();
 
-    <Section className={styles.contact} name="Contact">
-      <div className={styles.dual}>
-        {resume.basics?.email && (
-          <Anchor href={`mailto:${resume.basics.email}`}>
-            <h5>
-              <AtSign />
-              {resume.basics.email}
-              <ExternalLink />
-            </h5>
-          </Anchor>
-        )}
-        {resume.basics?.url && (
-          <Anchor href={resume.basics.url}>
-            <h5>
-              <ExternalLinkIcon />
-              {resume.basics.url}
-              <ExternalLink />
-            </h5>
-          </Anchor>
-        )}
-        {resume.basics?.profiles?.map((profile) => {
-          if (!profile.url) return null;
+  return (
+    <div className={styles.page}>
+      <header className={styles.header}>
+        {resume.basics?.name && <h1>{resume.basics.name}</h1>}
+        {resume.basics?.label && <h2>{resume.basics.label}</h2>}
+      </header>
 
-          let Icon: FC | null = null;
-
-          switch (profile.network?.toLowerCase()) {
-            case 'github':
-              Icon = GitHub;
-              break;
-
-            case 'instagram':
-              Icon = Instagram;
-              break;
-
-            case 'linkedin':
-              Icon = Linkedin;
-              break;
-
-            case 'twitter':
-              Icon = Twitter;
-              break;
-
-            default:
-              break;
-          }
-
-          return (
-            <Anchor href={profile.url} key={profile.network}>
+      <Section className={styles.contact} name="Contact">
+        <div className={styles.dual}>
+          {resume.basics?.email && (
+            <Anchor href={`mailto:${resume.basics.email}`}>
               <h5>
-                {Icon && <Icon />}/{profile.username}
+                <AtSign />
+                {resume.basics.email}
                 <ExternalLink />
               </h5>
             </Anchor>
-          );
-        })}
-      </div>
-    </Section>
-
-    <section className={styles.skills}>
-      {resume.skills?.map((skill) => (
-        <Section key={skill.name} name={skill.name}>
-          <div className={styles.dual}>
-            {skill.keywords?.map((keyword) => (
-              <h5 className={styles.skill} key={keyword}>
-                {keyword}
+          )}
+          {resume.basics?.url && (
+            <Anchor href={resume.basics.url}>
+              <h5>
+                <ExternalLinkIcon />
+                {resume.basics.url}
+                <ExternalLink />
               </h5>
-            ))}
-          </div>
-        </Section>
-      ))}
-    </section>
+            </Anchor>
+          )}
+          {resume.basics?.profiles?.map((profile) => {
+            if (!profile.url) return null;
 
-    <Section className={styles.languages} name="Languages">
-      <div className={styles.list}>
-        {resume?.languages?.map((language) => (
-          <div className={styles.language} key={language.language}>
-            <h5>{language.language}</h5>
-            <span>{language.fluency}</span>
+            let Icon: FC | null = null;
+
+            switch (profile.network?.toLowerCase()) {
+              case 'github':
+                Icon = GitHub;
+                break;
+
+              case 'instagram':
+                Icon = Instagram;
+                break;
+
+              case 'linkedin':
+                Icon = Linkedin;
+                break;
+
+              case 'twitter':
+                Icon = Twitter;
+                break;
+
+              default:
+                break;
+            }
+
+            return (
+              <Anchor href={profile.url} key={profile.network}>
+                <h5>
+                  {Icon && <Icon />}/{profile.username}
+                  <ExternalLink />
+                </h5>
+              </Anchor>
+            );
+          })}
+        </div>
+      </Section>
+
+      <section className={styles.skills}>
+        {resume.skills?.map((skill) => (
+          <Section key={skill.name} name={skill.name}>
+            <div className={styles.dual}>
+              {skill.keywords?.map((keyword) => (
+                <h5 className={styles.skill} key={keyword}>
+                  {keyword}
+                </h5>
+              ))}
+            </div>
+          </Section>
+        ))}
+      </section>
+
+      <Section className={styles.languages} name="Languages">
+        <div className={styles.list}>
+          {resume?.languages?.map((language) => (
+            <div className={styles.language} key={language.language}>
+              <h5>{language.language}</h5>
+              <span>{language.fluency}</span>
+            </div>
+          ))}
+        </div>
+      </Section>
+
+      <Section className={styles.experience} name="Experience">
+        {resume.work?.map((work) => (
+          <div className={styles.work} key={work.name}>
+            <h6>{`${work.startDate} - ${work.endDate || 'Present'}`}</h6>
+            <h4>
+              <Anchor href={work.url}>
+                {work.name}
+                <ExternalLink />
+              </Anchor>
+            </h4>
+            <h5>{work.position}</h5>
+            <ul>{work.highlights?.map((highlight) => <li key={highlight}>{highlight}</li>)}</ul>
           </div>
         ))}
-      </div>
-    </Section>
-
-    <Section className={styles.experience} name="Experience">
-      {resume.work?.map((work) => (
-        <div className={styles.work} key={work.name}>
-          <h6>{`${work.startDate} - ${work.endDate || 'Present'}`}</h6>
-          <h4>
-            <Anchor href={work.url}>
-              {work.name}
-              <ExternalLink />
-            </Anchor>
-          </h4>
-          <h5>{work.position}</h5>
-          <ul>{work.highlights?.map((highlight) => <li key={highlight}>{highlight}</li>)}</ul>
+      </Section>
+      <div className="sticky bottom-2 flex justify-center print:hidden">
+        <div className="glass p-2">
+          <Link className="button green text-xs" href="/resume/custom">
+            {t('Generate your own resume')}
+          </Link>
         </div>
-      ))}
-    </Section>
-  </div>
-);
+      </div>
+    </div>
+  );
+};
