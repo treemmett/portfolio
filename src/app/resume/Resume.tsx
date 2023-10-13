cc68a70ae58efc2025bdd8607f0c6fcc8d12da2c
Tregan <tregan@tregan.me>
2023-10-13 05:36:55 +0300

    fix(resume): use tailwind

--- src/app/resume/Resume.tsx
+++ src/app/resume/Resume.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import cx from 'classnames';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { FC, PropsWithChildren } from 'react';
@@ -26,12 +27,14 @@ const Section: FC<PropsWithChildren<{ className?: string; name?: string }>> = ({
 }) => (
   <section className={className}>
     <h3>{name}</h3>
-    <hr />
+    <hr className="my-2" />
     {children}
   </section>
 );
 
-const ExternalLink: FC = () => <ExternalLinkIcon className={styles['external-link']} />;
+const ExternalLink: FC<{ className?: string }> = ({ className }) => (
+  <ExternalLinkIcon className={cx(className, 'mb-auto ml-1 h-3 w-3 print:hidden')} />
+);
 
 export const Resume: FC<{ resume: ResumeType }> = ({ resume }) => {
   const { t } = useTranslation();
@@ -41,36 +44,32 @@ export const Resume: FC<{ resume: ResumeType }> = ({ resume }) => {
 
   return (
     <>
-      <div className={styles.page}>
+      <div className={cx(styles.page, 'm-auto grid w-full max-w-6xl gap-8 p-8')}>
         <header className={styles.header}>
           {resume.basics?.name && <h1>{resume.basics.name}</h1>}
           {resume.basics?.label && <h2>{resume.basics.label}</h2>}
         </header>
 
         <Section className={styles.contact} name="Contact">
-          <div className={styles.dual}>
+          <div className="grid grid-cols-2 gap-4 pt-3 md:grid-cols-3">
             {resume.basics?.email && (
-              <Anchor href={`mailto:${resume.basics.email}`}>
-                <h5>
-                  <AtSign />
-                  {resume.basics.email}
-                  <ExternalLink />
-                </h5>
+              <Anchor className="flex items-center" href={`mailto:${resume.basics.email}`}>
+                <AtSign className="mr-2 stroke-1" />
+                {resume.basics.email}
+                <ExternalLink />
               </Anchor>
             )}
             {resume.basics?.url && (
-              <Anchor href={resume.basics.url}>
-                <h5>
-                  <ExternalLinkIcon />
-                  {resume.basics.url}
-                  <ExternalLink />
-                </h5>
+              <Anchor className="flex items-center" href={resume.basics.url}>
+                <ExternalLinkIcon className="mr-2 stroke-1" />
+                {resume.basics.url}
+                <ExternalLink />
               </Anchor>
             )}
             {resume.basics?.profiles?.map((profile) => {
               if (!profile.url) return null;
 
-              let Icon: FC | null = null;
+              let Icon: FC<{ className?: string }> | null = null;
 
               switch (profile.network?.toLowerCase()) {
                 case 'github':
@@ -94,35 +93,33 @@ export const Resume: FC<{ resume: ResumeType }> = ({ resume }) => {
               }
 
               return (
-                <Anchor href={profile.url} key={profile.network}>
-                  <h5>
-                    {Icon && <Icon />}/{profile.username}
-                    <ExternalLink />
-                  </h5>
+                <Anchor className="flex items-center" href={profile.url} key={profile.network}>
+                  {Icon && <Icon className="mr-2 stroke-1" />}/{profile.username}
+                  <ExternalLink />
                 </Anchor>
               );
             })}
           </div>
         </Section>
 
-        <section className={styles.skills}>
+        <div className={cx(styles.skills, 'flex flex-col gap-4')}>
           {resume.skills?.map((skill) => (
             <Section key={skill.name} name={skill.name}>
-              <div className={styles.dual}>
+              <div className="grid grid-cols-2  gap-x-4 gap-y-2 pt-2 lg:grid-cols-3">
                 {skill.keywords?.map((keyword) => (
-                  <h5 className={styles.skill} key={keyword}>
+                  <h5 className="self-center" key={keyword}>
                     {keyword}
                   </h5>
                 ))}
               </div>
             </Section>
           ))}
-        </section>
+        </div>
 
         <Section className={styles.languages} name="Languages">
-          <div className={styles.list}>
+          <div className="flex justify-between">
             {resume?.languages?.map((language) => (
-              <div className={styles.language} key={language.language}>
+              <div key={language.language}>
                 <h5>{language.language}</h5>
                 <span>{language.fluency}</span>
               </div>
@@ -130,21 +127,37 @@ export const Resume: FC<{ resume: ResumeType }> = ({ resume }) => {
           </div>
         </Section>
 
-        <Section className={styles.experience} name="Experience">
-          {resume.work?.map((work) => (
-            <div className={styles.work} key={work.name}>
-              <h6>{`${work.startDate} - ${work.endDate || 'Present'}`}</h6>
-              <h4>
-                <Anchor href={work.url}>
-                  {work.name}
-                  <ExternalLink />
-                </Anchor>
-              </h4>
-              <h5>{work.position}</h5>
-              <ul>{work.highlights?.map((highlight) => <li key={highlight}>{highlight}</li>)}</ul>
-            </div>
-          ))}
-        </Section>
+        <div className={styles.experience}>
+          <h3>Experience</h3>
+          <hr className="my-2" />
+          <div className="flex flex-col gap-6">
+            {resume.work?.map((work) => (
+              <div key={work.name}>
+                <div className="flex justify-between">
+                  <h4>
+                    {work.url ? (
+                      <Anchor className="inline-flex items-center" href={work.url}>
+                        {work.name}
+                        <ExternalLink className="inline-block" />
+                      </Anchor>
+                    ) : (
+                      <div>{work.name}</div>
+                    )}
+                  </h4>
+                  <h6>{`${work.startDate} - ${work.endDate || 'Present'}`}</h6>
+                </div>
+                <h5 className="mb-2">{work.position}</h5>
+                <ul>
+                  {work.highlights?.map((highlight) => (
+                    <li className="list-disc" key={highlight}>
+                      {highlight}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
       <div className="sticky bottom-2 flex justify-center print:hidden">
         <div className="glass flex gap-2 p-2">
--- src/app/resume/loading.tsx
+++ src/app/resume/loading.tsx
@@ -21,14 +21,14 @@ export default async function ResumeLoading(
   const skeleton = animated ? 'skeleton' : 'skeleton reduce';
 
   return (
-    <div className={styles.page}>
+    <div className={cx(styles.page, 'm-auto grid w-full max-w-6xl gap-8 p-8')}>
       <header className={styles.header}>
         <div className={`${skeleton} h-4 w-1/2`} />
         <div className={`${skeleton} mt-2 h-4 w-3/4`} />
       </header>
 
       <Section className={styles.contact} name="Contact">
-        <div className={styles.dual}>
+        <div className="grid grid-cols-2 gap-4 pt-3 md:grid-cols-3">
           {new Array(6).fill(null).map((_, i) => (
             <div className="flex items-center" key={i}>
               <div className={`rounded-full ${skeleton} h-8 w-8`} />
@@ -43,7 +43,7 @@ export default async function ResumeLoading(
           <div className="mb-8" key={i}>
             <div className={`${skeleton} h-4 w-2/5`} />
             <hr className="mt-2" />
-            <div className={styles.dual}>
+            <div className="grid grid-cols-2  gap-x-4 gap-y-2 pt-2 lg:grid-cols-3">
               {new Array(5).fill(null).map((__, j) => (
                 <div className={`${skeleton} h-4 w-3/5`} key={j} />
               ))}
@@ -53,7 +53,7 @@ export default async function ResumeLoading(
       </section>
 
       <Section className={cx(styles.languages, 'relative')} name="Languages">
-        <div className={styles.list}>
+        <div className="flex justify-between">
           {new Array(3).fill(null).map((_, i) => (
             <div className={cx(styles.language, 'mt-2 w-full')} key={i}>
               <div className={`${skeleton} h-3 w-1/3`} />
@@ -63,18 +63,24 @@ export default async function ResumeLoading(
         </div>
       </Section>
 
-      <Section className={styles.experience} name="Experience">
-        {new Array(3).fill({}).map((_, i) => (
-          <div className={styles.work} key={i}>
-            <div className={`${skeleton} ml-auto mt-3 h-4 w-1/4`} />
-            <div className={`${skeleton} mt-2 h-4 w-1/3`} />
-            <div className={`${skeleton} mt-2 h-4 w-2/5`} />
-            <div className={`${skeleton} mt-2 h-4 w-full`} />
-            <div className={`${skeleton} mt-2 h-4 w-full`} />
-            <div className={`${skeleton} mt-2 h-4 w-3/4`} />
-          </div>
-        ))}
-      </Section>
+      <div>
+        <h5>Experience</h5>
+        <hr className="my-2" />
+        <div className="flex flex-col gap-6">
+          {new Array(5).fill({}).map((_, i) => (
+            <div className={styles.work} key={i}>
+              <div className="flex justify-between">
+                <div className={`${skeleton} mt-2 h-4 w-1/3`} />
+                <div className={`${skeleton} ml-auto mt-3 h-4 w-1/4`} />
+              </div>
+              <div className={`${skeleton} mt-2 h-4 w-2/5`} />
+              <div className={`${skeleton} mt-2 h-4 w-full`} />
+              <div className={`${skeleton} mt-2 h-4 w-full`} />
+              <div className={`${skeleton} mt-2 h-4 w-3/4`} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
