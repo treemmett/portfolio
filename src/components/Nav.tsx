ffcd71a868c379dd29340131b78cee17b2682cf5
Tregan <tregan@tregan.me>
2023-10-13 07:44:39 +0300

    feat(nav): add resume link

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,38 +1,42 @@
 import classNames from 'classnames';
+import Link from 'next/link';
 import React from 'react';
 import { Anchor } from './Anchor';
 import styles from './Nav.module.scss';
 import { UserButton } from './UserButton';
 import { getSite } from '@lib/getSite';
+import { useTranslation } from '@utils/translation';
 
 export async function Nav({ className }: { className?: string }) {
   const site = await getSite();
+  const { t } = useTranslation();
 
   return (
     <header className={classNames(styles.nav, className)}>
-      <h1>{site?.name}</h1>
+      <h1>{site.name}</h1>
 
       <nav>
-        {site?.facebook && (
+        {site.resumeUrl && <Link href="/resume">{t('Resume')}</Link>}
+        {site.facebook && (
           <Anchor href={`https://facebook.com/${encodeURIComponent(site.facebook)}`}>
             Facebook
           </Anchor>
         )}
-        {site?.github && (
+        {site.github && (
           <Anchor href={`https://github.com/${encodeURIComponent(site.github)}`}>GitHub</Anchor>
         )}
-        {site?.imdb && (
+        {site.imdb && (
           <Anchor href={`https://www.imdb.com/name/${encodeURIComponent(site.imdb)}/`}>IMDb</Anchor>
         )}
-        {site?.instagram && (
+        {site.instagram && (
           <Anchor href={`https://www.instagram.com/${encodeURIComponent(site.instagram)}/`}>
             Instagram
           </Anchor>
         )}
-        {site?.twitter && (
+        {site.twitter && (
           <Anchor href={`https://twitter.com/${encodeURIComponent(site.twitter)}/`}>Twitter</Anchor>
         )}
-        {site?.linkedIn && (
+        {site.linkedIn && (
           <Anchor href={`https://www.linkedin.com/in/${encodeURIComponent(site.linkedIn)}/`}>
             LinkedIn
           </Anchor>
