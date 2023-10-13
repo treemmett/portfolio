a3adc1221493dc44390af03cb548128b64b8b70a
Tregan <tregan@tregan.me>
2023-10-13 08:03:40 +0300

    refactor(nav): use full client

--- src/app/navbar/Menu.tsx
+++ src/app/navbar/Menu.tsx
@@ -1,57 +1,65 @@
 'use client';
 
 import { Site } from '@prisma/client';
+import classNames from 'classnames';
 import Link from 'next/link';
 import { FC } from 'react';
+import styles from './Nav.module.scss';
 import { Anchor } from '@components/Anchor';
 import { useUser } from '@lib/user';
 import { useTranslation } from '@utils/translation';
 
-export const Menu: FC<{ site: Site }> = ({ site }) => {
+export const Menu: FC<{ className?: string; site: Site }> = ({ className, site }) => {
   const { t } = useTranslation();
   const { isLoggingIn, login, logout, user } = useUser();
 
   return (
-    <nav>
-      {site.resumeUrl && <Link href="/resume">{t('Resume')}</Link>}
-      {site.facebook && (
-        <Anchor href={`https://facebook.com/${encodeURIComponent(site.facebook)}`}>Facebook</Anchor>
-      )}
-      {site.github && (
-        <Anchor href={`https://github.com/${encodeURIComponent(site.github)}`}>GitHub</Anchor>
-      )}
-      {site.imdb && (
-        <Anchor href={`https://www.imdb.com/name/${encodeURIComponent(site.imdb)}/`}>IMDb</Anchor>
-      )}
-      {site.instagram && (
-        <Anchor href={`https://www.instagram.com/${encodeURIComponent(site.instagram)}/`}>
-          Instagram
-        </Anchor>
-      )}
-      {site.twitter && (
-        <Anchor href={`https://twitter.com/${encodeURIComponent(site.twitter)}/`}>Twitter</Anchor>
-      )}
-      {site.linkedIn && (
-        <Anchor href={`https://www.linkedin.com/in/${encodeURIComponent(site.linkedIn)}/`}>
-          LinkedIn
-        </Anchor>
-      )}
-      {user ? (
-        <>
-          {site?.ownerId === user.id ? (
-            <Link href="/settings">{t('Settings')}</Link>
-          ) : (
-            <Link href={`/u/${encodeURIComponent(user.username)}`}>{t('My Site')}</Link>
-          )}
-          <button onClick={() => logout()} type="button">
-            {t('Logout')}
+    <header className={classNames(styles.nav, className)}>
+      <h1>{site.name}</h1>
+
+      <nav>
+        {site.resumeUrl && <Link href="/resume">{t('Resume')}</Link>}
+        {site.facebook && (
+          <Anchor href={`https://facebook.com/${encodeURIComponent(site.facebook)}`}>
+            Facebook
+          </Anchor>
+        )}
+        {site.github && (
+          <Anchor href={`https://github.com/${encodeURIComponent(site.github)}`}>GitHub</Anchor>
+        )}
+        {site.imdb && (
+          <Anchor href={`https://www.imdb.com/name/${encodeURIComponent(site.imdb)}/`}>IMDb</Anchor>
+        )}
+        {site.instagram && (
+          <Anchor href={`https://www.instagram.com/${encodeURIComponent(site.instagram)}/`}>
+            Instagram
+          </Anchor>
+        )}
+        {site.twitter && (
+          <Anchor href={`https://twitter.com/${encodeURIComponent(site.twitter)}/`}>Twitter</Anchor>
+        )}
+        {site.linkedIn && (
+          <Anchor href={`https://www.linkedin.com/in/${encodeURIComponent(site.linkedIn)}/`}>
+            LinkedIn
+          </Anchor>
+        )}
+        {user ? (
+          <>
+            {site?.ownerId === user.id ? (
+              <Link href="/settings">{t('Settings')}</Link>
+            ) : (
+              <Link href={`/u/${encodeURIComponent(user.username)}`}>{t('My Site')}</Link>
+            )}
+            <button onClick={() => logout()} type="button">
+              {t('Logout')}
+            </button>
+          </>
+        ) : (
+          <button disabled={isLoggingIn} onClick={() => login()} type="button">
+            {isLoggingIn ? `${t('Logging in')}...` : t('Login')}
           </button>
-        </>
-      ) : (
-        <button disabled={isLoggingIn} onClick={() => login()} type="button">
-          {isLoggingIn ? `${t('Logging in')}...` : t('Login')}
-        </button>
-      )}
-    </nav>
+        )}
+      </nav>
+    </header>
   );
 };
--- src/app/navbar/Nav.tsx
+++ src/app/navbar/Nav.tsx
@@ -1,17 +1,8 @@
-import classNames from 'classnames';
-import React from 'react';
 import { Menu } from './Menu';
-import styles from './Nav.module.scss';
 import { getSite } from '@lib/getSite';
 
 export async function Nav({ className }: { className?: string }) {
   const site = await getSite();
 
-  return (
-    <header className={classNames(styles.nav, className)}>
-      <h1>{site.name}</h1>
-
-      <Menu site={site} />
-    </header>
-  );
+  return <Menu className={className} site={site} />;
 }
