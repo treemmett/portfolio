096236b58a503e1661d756ed524a4c318b7c0dab
Tregan <tregan@tregan.me>
2023-10-13 06:38:38 +0300

    fix(posts): order by date

--- src/lib/getPosts.ts
+++ src/lib/getPosts.ts
@@ -4,6 +4,9 @@ import { prisma } from '@utils/prisma';
 export function getPosts() {
   return prisma.post.findMany({
     include: { photo: true, user: true },
+    orderBy: {
+      created: 'desc',
+    },
     where: { user: { username: Config.DEFAULT_USER } },
   });
 }
