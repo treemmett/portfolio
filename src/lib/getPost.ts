af0a9d60372e77a5e9803a161646a01750a4c344
Tregan <tregan@tregan.me>
2023-10-31 02:06:54 +0200

    perf(getPost): use getPosts cache

--- src/lib/getPost.ts
+++ src/lib/getPost.ts
@@ -1,26 +1,13 @@
 import { notFound } from 'next/navigation';
-import { cache } from 'react';
-import validator from 'validator';
-import { Config } from '@utils/config';
-import { prisma } from '@utils/prisma';
+import { getPosts } from './getPosts';
 
-export const getPost = cache(async (id: string) => {
-  if (!validator.isUUID(id)) return notFound();
+export async function getPost(id: string) {
+  const posts = await getPosts();
+  const post = posts.find((p) => p.id === id);
 
-  const post = await prisma.post.findFirst({
-    include: { photo: true, user: true },
-    where: { id, user: { username: Config.DEFAULT_USER } },
-  });
+  if (!post) {
+    notFound();
+  }
 
-  if (!post) return notFound();
-
-  return {
-    ...post,
-    photo: {
-      ...post.photo,
-      url: Config.CDN_URL
-        ? `${Config.CDN_URL}/${post.photo?.id}`
-        : `${Config.S3_URL}/${Config.S3_BUCKET}/${post.photo?.id}`,
-    },
-  };
-});
+  return post;
+}
